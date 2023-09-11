c7ae99b00ccada17373a1d3f7d2bed8f2cf67fd3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-09-11 23:28:58 +0530

    Create 1282_Group_the_People_Given_the_Group_Size_They_Belong_To.cpp

---
+++ 1282_Group_the_People_Given_the_Group_Size_They_Belong_To.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> groups;
+        vector<vector<int>> result;
+
+        for (int i = 0; i < groupSizes.size(); i++) {
+            int size = groupSizes[i];
+            groups[size].push_back(i);
+
+            if (groups[size].size() == size) {
+                result.push_back(groups[size]);
+                groups[size].clear();
+            }
+        }
+
+        return result;
+    }
+};
