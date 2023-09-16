81679d673e91945c302a922d3c888db251d6e2c7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-09-17 00:54:08 +0530

    Create 1631_Path_With_Minimum_Effort.cpp

--- 1631_Path_With_Minimum_Effort.cpp
+++ 1631_Path_With_Minimum_Effort.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> P;
-    vector<vector<int>> dirs = {
-                {-1,0},
-        {0,-1},         {0,1},
-                {1, 0}
-    };
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
-        
-        auto isSafe = [&](int x, int y) {
-            return x>=0 && x<m && y>=0 && y<n;
-        };
-        
-        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
-        priority_queue<P, vector<P>, greater<P>> pq;
-        
-        pq.push({0, {0, 0}});
-        result[0][0] = 0;
-  
-        while(!pq.empty()) {
-            int diff  = pq.top().first;
-            auto node = pq.top().second;
-            pq.pop();
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
 
-            int x = node.first;
-            int y = node.second;
+        priority_queue<pair<int, pair<int, int>>> q;
+        q.push({0,{0, 0}});
+        dist[0][0] = 0;
+
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+
+        while(!q.empty())
+        {
+            int curr = -q.top().first;
+            int x = q.top().second.first;
+            int y = q.top().second.second;
+            q.pop();
+
+            if(x == n-1 && y == m-1) return curr;
             
-            if(x == m-1 && y == n-1)
-                return diff;
-            
-	    for(auto dir:dirs) {
-		int x_   = x + dir[0];
-		int y_   = y + dir[1];
+            for(int i = 0; i < 4; i++)
+            {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
 
-		if(isSafe(x_, y_)) {
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m)
+                {
+                    int diff = max(abs(heights[newX][newY] - heights[x][y]), curr);
+                    if(dist[newX][newY] > diff)
+                    {
+                        dist[newX][newY] = diff;
+                        q.push({-diff,{newX,newY}});
+                    }
+                }
+            }
 
-		    int newDiff = max(diff, abs(heights[x][y] - heights[x_][y_]));
-		    if(result[x_][y_] > newDiff) {
-			result[x_][y_] = newDiff;
-			pq.push({result[x_][y_], {x_, y_}});
-		    }
-		}
-	     }
+
         }
-   
-        return result[m-1][n-1];
-
+        return 0;
     }
 };
