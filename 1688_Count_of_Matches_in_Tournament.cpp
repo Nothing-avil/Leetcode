a9c8248822d532278a92c423fbf8742ff34b237b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-12-05 22:32:01 +0530

    Create 1688_Count_of_Matches_in_Tournament.cpp

---
+++ 1688_Count_of_Matches_in_Tournament.cpp
@@ -0,0 +1,9 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        return n-1;
+    }
+};
