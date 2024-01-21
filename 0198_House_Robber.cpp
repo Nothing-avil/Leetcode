8dea02cfd9ac0c145f6d4a16180b1d0b2e3a11fd
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-21 23:12:09 +0530

    Create 0198_House_Robber.cpp

---
+++ 0198_House_Robber.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        int n = nums.size();
+        int f[n + 1];
+        memset(f, 0, sizeof(f));
+        f[1] = nums[0];
+        for (int i = 2; i <= n; ++i) {
+            f[i] = max(f[i - 1], f[i - 2] + nums[i - 1]);
+        }
+        return f[n];
+    }
+};
