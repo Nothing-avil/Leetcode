c2b07da1a243c9ed8a564dd01d51ba6f44f16954
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-29 08:16:58 +0530

    Create 0232_Implement_Queue_using_Stacks.cpp

---
+++ 0232_Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,49 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class MyQueue {
+public:
+    stack<int>s1;
+    stack<int>s2; 
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int ans=s2.top();
+        s2.pop();
+        return ans;
+    }
+    
+    int peek() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
