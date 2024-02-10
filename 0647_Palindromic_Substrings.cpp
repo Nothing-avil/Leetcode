d6208f6d1565d92c941f13c688d81a61b31a93cf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-10 23:14:43 +0530

    Create 0647_Palindromic_Substrings.cpp

---
+++ 0647_Palindromic_Substrings.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class Solution {
+public:
+    int countSubstrings(string s) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        int ans = 0;
+        int n = s.size();
+        for (int k = 0; k < n * 2 - 1; ++k) {
+            int i = k / 2, j = (k + 1) / 2;
+            while (~i && j < n && s[i] == s[j]) {
+                ++ans;
+                --i;
+                ++j;
+            }
+        }
+        return ans;
+    }
+};
