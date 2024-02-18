74be0c8a9b5fb6fc8882a062cac5f930f8729932
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-18 23:53:52 +0530

    Create 2402_Meeting_Rooms_III.cpp

---
+++ 2402_Meeting_Rooms_III.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+
+using ll = long long;
+using pii = pair<ll, int>;
+
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+        priority_queue<int, vector<int>, greater<int>> idle;
+        priority_queue<pii, vector<pii>, greater<pii>> busy;
+        for (int i = 0; i < n; ++i) idle.push(i);
+        vector<int> cnt(n);
+        sort(meetings.begin(), meetings.end());
+        for (auto& v : meetings) {
+            int s = v[0], e = v[1];
+            while (!busy.empty() && busy.top().first <= s) {
+                idle.push(busy.top().second);
+                busy.pop();
+            }
+            int i = 0;
+            if (!idle.empty()) {
+                i = idle.top();
+                idle.pop();
+                busy.push({e, i});
+            } else {
+                auto x = busy.top();
+                busy.pop();
+                i = x.second;
+                busy.push({x.first + e - s, i});
+            }
+            ++cnt[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (cnt[ans] < cnt[i]) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
