07da97907be22377729ab0b67f0588ee505a9e4d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-03-03 12:31:53 +0530

    Create 0019_Remove_Nth_Node_From_End_of_List.cpp

---
+++ 0019_Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        while (n--) {
+            fast = fast->next;
+        }
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
