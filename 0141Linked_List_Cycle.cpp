775e86263d457acb52f8c169179d60937cd23c6b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-03-06 21:34:29 +0530

    create 0141_Linked_List_Cycle.cpp

--- 0141Linked_List_Cycle.cpp
+++ 0141Linked_List_Cycle.cpp
@@ -1,16 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         ListNode* slow = head;
         ListNode* fast = head;
-
         while (fast && fast->next) {
         slow = slow->next;
         fast = fast->next->next;
         if (slow == fast)
             return true;
         }
-
         return false;
     }
 };
