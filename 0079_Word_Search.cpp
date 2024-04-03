dfc5f6a0e0d8c09b36d09849c8da713cee25fe2b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-03 20:39:24 +0530

    Create 0079_Word_Search.cpp

---
+++ 0079_Word_Search.cpp
@@ -0,0 +1,42 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);  
+    cin.tie(nullptr);            
+    cout.tie(nullptr);           
+    return 'c';                  
+}();
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        function<bool(int, int, int)> dfs = [&](int i, int j, int k) -> bool {
+            if (k == word.size() - 1) {
+                return board[i][j] == word[k];
+            }
+            if (board[i][j] != word[k]) {
+                return false;
+            }
+            char c = board[i][j];
+            board[i][j] = '0';
+            for (int u = 0; u < 4; ++u) {
+                int x = i + dirs[u], y = j + dirs[u + 1];
+                if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] != '0' && dfs(x, y, k + 1)) {
+                    return true;
+                }
+            }
+            board[i][j] = c;
+            return false;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
