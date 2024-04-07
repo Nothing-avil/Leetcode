911c0b0c34d5440d5b5a0d92228ae5268c94465a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-07 22:50:10 +0530

    Create 0678_Valid_Parenthesis_String.cpp

---
+++ 0678_Valid_Parenthesis_String.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int op=0;
+        for(int i=0;i<s.size(); i++){
+            if(s[i]!=')'){
+                op++;
+            }
+            else if(op){
+                op--;
+            }
+            else{
+                return false;
+            }
+        }
+        op=0;
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i]!='('){
+                op++;
+            }
+            else if(op){
+                op--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
