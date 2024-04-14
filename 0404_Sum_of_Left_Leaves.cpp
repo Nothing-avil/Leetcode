a7b9d8c02599b23be6f76da7bc3b12aff4edf41d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-14 22:57:16 +0530

    Create 0404_Sum_of_Left_Leaves.cpp

---
+++ 0404_Sum_of_Left_Leaves.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+
+    int solve(TreeNode* root, bool tleft){
+        if(root==NULL){
+            return 0;
+        }
+        if(tleft && root->left==NULL && root->right==NULL){
+            return root->val;
+        }
+
+        return solve(root->left, true) + solve(root->right, false);
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        return solve(root, false);
+    }
+};
