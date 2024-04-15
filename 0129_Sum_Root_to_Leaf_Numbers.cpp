4f9a010028a0d3893dc028a8c37107eed4d49e91
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-15 21:12:19 +0530

    Create 0129_Sum_Root_to_Leaf_Numbers.cpp

---
+++ 0129_Sum_Root_to_Leaf_Numbers.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return d(root, 0);
+    }
+
+    int d(TreeNode* root, int a){
+        if(!root){
+            return 0;
+        }
+        a= a*10 + root->val;
+        if(!root->left && !root->right){
+            return a;
+        }
+        return d(root->left, a) + d(root->right, a);
+    }
+};
