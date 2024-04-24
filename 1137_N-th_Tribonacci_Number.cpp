95ace5f6d287a53a58b3305a6c6bc8b367fdae6f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-24 22:11:23 +0530

    Create 1137_N-th_Tribonacci_Number.cpp

---
+++ 1137_N-th_Tribonacci_Number.cpp
@@ -0,0 +1,28 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        int a=0, b=1, c=1;
+        for(int i=0; i<=n-3; i++){
+            int d= a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return c;
+    }
+};
