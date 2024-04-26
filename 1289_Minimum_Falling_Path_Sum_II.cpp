f60bc17c6069c2caf342cfdd2e2b5afd4c6ddc68
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-26 23:52:26 +0530

    Create 1289_Minimum_Falling_Path_Sum_II.cpp

---
+++ 1289_Minimum_Falling_Path_Sum_II.cpp
@@ -0,0 +1,30 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int f[n + 1][n];
+        memset(f, 0, sizeof(f));
+        const int inf = 1 << 30;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int x = inf;
+                for (int k = 0; k < n; ++k) {
+                    if (k != j) {
+                        x = min(x, f[i - 1][k]);
+                    }
+                }
+                f[i][j] = grid[i - 1][j] + (x == inf ? 0 : x);
+            }
+        }
+        return *min_element(f[n], f[n] + n);
+    }
+};
