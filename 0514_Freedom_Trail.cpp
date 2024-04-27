0c819b238afec697ca602a324eb333e223ddd5b4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-04-27 23:33:43 +0530

    Create 0514_Freedom_Trail.cpp

---
+++ 0514_Freedom_Trail.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findRotateSteps(string ring, string key) {
+        int m = key.size(), n = ring.size();
+        vector<int> pos[26];
+        for (int j = 0; j < n; ++j) {
+            pos[ring[j] - 'a'].push_back(j);
+        }
+        int f[m][n];
+        memset(f, 0x3f, sizeof(f));
+        for (int j : pos[key[0] - 'a']) {
+            f[0][j] = min(j, n - j) + 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j : pos[key[i] - 'a']) {
+                for (int k : pos[key[i - 1] - 'a']) {
+                    f[i][j] = min(f[i][j], f[i - 1][k] + min(abs(j - k), n - abs(j - k)) + 1);
+                }
+            }
+        }
+        int ans = 1 << 30;
+        for (int j : pos[key[m - 1] - 'a']) {
+            ans = min(ans, f[m - 1][j]);
+        }
+        return ans;
+    }
+};
