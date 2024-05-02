2bd15884329abdb9f66f684a5e8fee53a26711f4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-02 23:46:37 +0530

    Create 2441_Largest__Positive_Integer_That_Exists_With_Its_Negative.cpp

---
+++ 2441_Largest__Positive_Integer_That_Exists_With_Its_Negative.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        int rea=-1;
+        for(int i : nums){
+            if(std::find(nums.begin(), nums.end(), (i*(-1))) != nums.end()){
+                rea = max(rea, i);
+            }
+        }
+        return rea;
+    }
+};
