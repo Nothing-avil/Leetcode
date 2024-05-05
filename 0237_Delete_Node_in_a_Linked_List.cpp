29e3cf51d26cd5e75185cd1922d91651534a2933
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-05 23:11:51 +0530

    Create 0237_Delete_Node_in_a_Linked_List.cpp

---
+++ 0237_Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val= node->next->val;
+        while(node->next->next!=NULL){
+            node->next->val= node->next->next->val;
+            node= node->next;
+        }
+        node->next =NULL;
+    }
+};
