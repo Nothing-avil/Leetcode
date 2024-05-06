29e1de0781f404271fe8403a090709c7f0be6a46
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-06 20:35:46 +0530

    Create 2487_Remove_Nodes_From_Linked_List.cpp

---
+++ 2487_Remove_Nodes_From_Linked_List.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if(head== NULL){
+            return head;
+        }
+        head->next= removeNodes(head->next);
+        if(head->next== NULL){
+            return head;
+        }
+        if(head->next->val > head->val){
+            return head->next;
+        }
+        return head;
+    }
+};
