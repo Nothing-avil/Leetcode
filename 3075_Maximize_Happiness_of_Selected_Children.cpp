60aca25c6c6a1e626f5258f9fd3773dab3cd46bc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-09 22:58:49 +0530

    Create 3075_Maximize_Happiness_of_Selected_Children.cpp

---
+++ 3075_Maximize_Happiness_of_Selected_Children.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end());
+        int c=0;
+        long long res=0;
+        for(int i=happiness.size()-1; i>=0; i--){
+            res+= max(0, happiness[i]-c);
+            c++;
+            if(c>=k){
+                break;
+            }
+        }
+        return res;
+    }
+};
