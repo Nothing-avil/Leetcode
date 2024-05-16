fa18ceeb312b0456623e35ee2b46ccce26cdeeb9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-16 23:16:01 +0530

    Create 2331_Evaluate_Boolean_Binary_Tree.cpp

---
+++ 2331_Evaluate_Boolean_Binary_Tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root->val==1){
+            return true;
+        }
+        if(root->val==0){
+            return false;
+        }
+
+        if(root->val==2){
+            return evaluateTree(root->left)||evaluateTree(root->right);
+        }
+        if(root->val==3){
+            return evaluateTree(root->left)&&evaluateTree(root->right);
+        }
+        return true;
+    }
+};
