488f036c6c34a00da4302369532eeeaf506982f4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-18 23:44:14 +0530

    Create 0979_Distribute_Coins_in_Binary_Tree.cpp

---
+++ 0979_Distribute_Coins_in_Binary_Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int m=0;
+
+    int solver(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int l= solver(root->left);
+        int r= solver(root->right);
+
+        m+=(abs(l)+abs(r));
+        return (l+r+root->val)-1;
+    }
+
+    int distributeCoins(TreeNode* root) {
+        solver(root);
+        return m;
+    }
+};
