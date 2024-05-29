df957601e7da075af4c61964aaa56361f07656a7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-05-29 23:28:42 +0530

    Create 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.cpp

---
+++ 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numSteps(string s) {
+        int ans = 0;
+        bool carry = false;
+        for (int i = s.size() - 1; i; --i) {
+            char c = s[i];
+            if (carry) {
+                if (c == '0') {
+                    c = '1';
+                    carry = false;
+                } else
+                    c = '0';
+            }
+            if (c == '1') {
+                ++ans;
+                carry = true;
+            }
+            ++ans;
+        }
+        if (carry) ++ans;
+        return ans;
+    }
+};
