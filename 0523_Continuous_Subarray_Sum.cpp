d1bbe549b67fea04aedf39db78513a5fc617fea4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-06-08 10:41:36 +0530

    Create 0523_Continuous_Subarray_Sum.cpp

---
+++ 0523_Continuous_Subarray_Sum.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int s = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            s += nums[i];
+            int r = s % k;
+            if (mp.count(r) && i - mp[r] >= 2) return true;
+            if (!mp.count(r)) mp[r] = i;
+        }
+        return false;
+    }
+};
