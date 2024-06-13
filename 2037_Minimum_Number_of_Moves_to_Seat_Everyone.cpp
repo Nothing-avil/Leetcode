ca37126c893f10c7e76021a345d0d786283e118b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-06-13 11:54:48 +0530

    Create 2037_Minimum_Number_of_Moves_to_Seat_Everyone.cpp

---
+++ 2037_Minimum_Number_of_Moves_to_Seat_Everyone.cpp
@@ -0,0 +1,21 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int c=0;
+        for(int i=0; i<students.size(); i++){
+            c+= abs(students[i]-seats[i]);
+        }
+        return c;
+    }
+};
