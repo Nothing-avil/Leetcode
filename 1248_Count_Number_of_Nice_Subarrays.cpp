70dd2ca07057ccc08f2ad6f234add183f4179433
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-06-22 17:57:31 +0530

    Create 1248_Count_Number_of_Nice_Subarrays.cpp\

---
+++ 1248_Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        // unordered_map<int, int> cnt;
+        vector<int> cnt(n + 1);
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for (int& v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += cnt[t - k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+};
