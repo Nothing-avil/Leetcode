0549e674a5183578eff093a3f9df29f183c63109
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-06-30 21:53:27 +0530

    Create 1579_Remove_Max_Number_of_Edges_to_Keep_Graph_Fully_Traversable.cpp

---
+++ 1579_Remove_Max_Number_of_Edges_to_Keep_Graph_Fully_Traversable.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("Ofast","inline","ffast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class UnionFind {
+public:
+    int cnt;
+
+    UnionFind(int n) {
+        p = vector<int>(n);
+        size = vector<int>(n, 1);
+        iota(p.begin(), p.end(), 0);
+        cnt = n;
+    }
+
+    bool unite(int a, int b) {
+        int pa = find(a - 1), pb = find(b - 1);
+        if (pa == pb) {
+            return false;
+        }
+        if (size[pa] > size[pb]) {
+            p[pb] = pa;
+            size[pa] += size[pb];
+        } else {
+            p[pa] = pb;
+            size[pb] += size[pa];
+        }
+        --cnt;
+        return true;
+    }
+
+    int find(int x) {
+        if (p[x] != x) {
+            p[x] = find(p[x]);
+        }
+        return p[x];
+    }
+
+private:
+    vector<int> p, size;
+};
+
+class Solution {
+public:
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        UnionFind ufa(n);
+        UnionFind ufb(n);
+        int ans = 0;
+        for (auto& e : edges) {
+            int t = e[0], u = e[1], v = e[2];
+            if (t == 3) {
+                if (ufa.unite(u, v)) {
+                    ufb.unite(u, v);
+                } else {
+                    ++ans;
+                }
+            }
+        }
+        for (auto& e : edges) {
+            int t = e[0], u = e[1], v = e[2];
+            ans += t == 1 && !ufa.unite(u, v);
+            ans += t == 2 && !ufb.unite(u, v);
+        }
+        return ufa.cnt == 1 && ufb.cnt == 1 ? ans : -1;
+    }
+};
