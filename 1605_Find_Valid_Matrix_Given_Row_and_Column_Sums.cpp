700e1ed17ad51f301f5a08b0b4764a2a94e18641
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-07-20 23:16:42 +0530

    Create 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp

---
+++ 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n= rowSum.size();
+        int m= colSum.size();
+        vector<vector<int>> res(n, vector<int>(m));
+        for(int i=0; i<n ;i++){
+            for(int j=0; j<m; j++){
+                res[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= res[i][j];
+                colSum[j] -= res[i][j];
+            }
+        }
+        return res;
+    }
+};
