63e437357a27355a52ce20c14d6cdfdc37614f51
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-07-30 23:04:22 +0530

    Create 1653_Minimum_Deletions_to_Make_String_Balanced.cpp

---
+++ 1653_Minimum_Deletions_to_Make_String_Balanced.cpp
@@ -0,0 +1,29 @@
+// ██╗  ██╗███████╗██╗     ██╗      ██████╗
+// ██║  ██║██╔════╝██║     ██║     ██╔═══██╗
+// ███████║█████╗  ██║     ██║     ██║   ██║
+// ██╔══██║██╔══╝  ██║     ██║     ██║   ██║
+// ██║  ██║███████╗███████╗███████╗╚██████╔╝▄█╗
+// ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int a=0, b=0;
+        for(char ch : s){
+            if(ch =='b'){
+                b++;
+            }
+            else{
+                a = min(a+1, b);
+            }
+        }
+        return a;
+    }
+};
