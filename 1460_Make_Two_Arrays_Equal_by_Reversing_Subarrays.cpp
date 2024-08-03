60f33a9036e22acd6c4243de275e19876e25440a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-03 12:34:58 +0530

    Create 1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.cpp

---
+++ 1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.cpp
@@ -0,0 +1,50 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+
+// Approch 1
+
+        int at[1001] ={0};
+        for(int i: arr){
+            at[i]++;
+        }
+        for(int i: target){
+            at[i]--;
+        }
+        for(int i=0; i<1001; i++){
+            if(at[i]){
+                return false;
+            }
+        }
+        return true;
+
+//Approch 2
+
+        unordered_map<int, int> mp;
+        for(int i : arr){
+            mp[i]++;
+        }
+        for(int i: target){
+            if(!mp[i]){
+                return false;
+            }
+            mp[i]--;
+        }
+        return true;
+    }
+};
