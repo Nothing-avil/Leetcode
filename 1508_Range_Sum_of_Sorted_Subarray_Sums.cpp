802fb3e9f2bc39fe5c12ed8fa5b9e16293c0db04
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-04 23:04:39 +0530

    Create 1508_Range_Sum_of_Sorted_Subarray_Sums.cpp

---
+++ 1508_Range_Sum_of_Sorted_Subarray_Sums.cpp
@@ -0,0 +1,42 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    const int mod = 1e9+7;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<int, vector<int>, greater<int>> qu;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            int c=0;
+            for(int j=i; j<n; j++){
+                c+=nums[j];
+                qu.push(c);
+            }
+        }
+        int cc= right-left+1;
+        left--;
+        while(left--){
+            int t= qu.top();
+            cout<<t<<endl;
+            qu.pop();
+        }
+        while(cc--){
+            int t= qu.top();
+            qu.pop();
+            ans =(ans+t)%mod;
+        }
+        return ans;
+    }
+};
