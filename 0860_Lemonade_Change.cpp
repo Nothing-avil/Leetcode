5e01df134dd614130b8c22232f2f786f11501893
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-15 23:43:04 +0530

    Create 0860_Lemonade_Change.cpp

---
+++ 0860_Lemonade_Change.cpp
@@ -0,0 +1,50 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int f5=0, t10=0, t20=0;
+        for(int i: bills){
+            if(i!=5){
+                if(i==10){
+                    if(f5<1){
+                        return false;
+                    }
+                    f5--;
+                    t10++;
+                }else{
+                    if(f5<3 && t10<1){
+                        return false;
+                    }
+                    if(t10<1){
+                        if(f5<3){
+                            return false;
+                        }
+                        f5-=3;
+                    }else{
+                        if(f5<1){
+                            return false;
+                        }
+                        t10--;
+                        f5--;
+                    }
+                }
+            }else{
+                f5++;
+            }
+        }
+        return true;
+    }
+};
