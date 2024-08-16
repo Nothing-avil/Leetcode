6175833cbac1190a48cfba089f9c02582223408f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-16 23:36:01 +0530

    Create 0624_Maximum_Distance_in_Arrays.cpp

---
+++ 0624_Maximum_Distance_in_Arrays.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int start = arrays[0].front();
+        int end = arrays[0].back();
+        int res=0;
+        for(int i=1; i<arrays.size(); i++){
+            int starts=arrays[i].front();
+            int ends = arrays[i].back();
+            res= max({res, abs(ends-start), abs(end-starts)});
+            start= min(start, starts);
+            end= max(end, ends);
+        }
+        return res;
+    }
+};
