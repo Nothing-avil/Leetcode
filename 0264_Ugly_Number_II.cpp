597b42c4213a9e5c2a8a4b2bbd0d81b1fe9bb838
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-18 18:40:21 +0530

    Create 0264_Ugly_Number_II.cpp

---
+++ 0264_Ugly_Number_II.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n+1);
+        int a2=1,a3=1,a5=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            int aa2= dp[a2]*2;
+            int aa3= dp[a3]*3;
+            int aa5= dp[a5]*5;
+            dp[i] = min({aa2,aa3,aa5});
+            if(dp[i]==aa2){
+                a2++;
+            }
+            if(dp[i]==aa3){
+                a3++;
+            }
+            if(dp[i]==aa5){
+                a5++;
+            }
+        }
+        return dp[n];
+    }
+};
