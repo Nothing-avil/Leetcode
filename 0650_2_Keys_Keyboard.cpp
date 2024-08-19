5df6e068892a06fd3a54c3f9324fc2e10f98ad50
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-19 23:10:04 +0530

    Create 0650_2_Keys_Keyboard.cpp

---
+++ 0650_2_Keys_Keyboard.cpp
@@ -0,0 +1,44 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+
+    int N;
+    int dp[1001][1001];
+    int solver(int A, int clip){
+        if(A==N){
+            return 0;
+        }
+        if(A>N){
+            return 100000;
+        }
+        if(dp[A][clip]!=-1){
+            return dp[A][clip];
+        }
+        int c= solver(A+A, A)+2;
+        int p= solver(A+clip, clip)+1;
+        return dp[A][clip]= min(c,p);
+    }
+
+
+    int minSteps(int n) {
+        if(n==1){
+            return 0;
+        }
+        memset(dp, -1, sizeof(dp));
+        N=n;
+        return 1+solver(1, 1);
+    }
+};
