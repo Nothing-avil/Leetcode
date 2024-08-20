05cc99432296e31fbb617727d62d00b4b688d126
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-20 23:44:48 +0530

    Create 1140_Stone_Game_II.cpp

---
+++ 1140_Stone_Game_II.cpp
@@ -0,0 +1,41 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        int s[n + 1];
+        s[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            s[i + 1] = s[i] + piles[i];
+        }
+        int f[n][n + 1];
+        memset(f, 0, sizeof f);
+        function<int(int, int)> dfs = [&](int i, int m) -> int {
+            if (m * 2 >= n - i) {
+                return s[n] - s[i];
+            }
+            if (f[i][m]) {
+                return f[i][m];
+            }
+            int res = 0;
+            for (int x = 1; x <= m << 1; ++x) {
+                res = max(res, s[n] - s[i] - dfs(i + x, max(x, m)));
+            }
+            return f[i][m] = res;
+        };
+        return dfs(0, 1);
+    }
+};
