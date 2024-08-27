025c9802a3b0fbd8e514a76ff641e035b220b368
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-08-27 23:18:35 +0530

    Create 1514_Path_with_Maximum_Probability.cpp

---
+++ 1514_Path_with_Maximum_Probability.cpp
@@ -0,0 +1,48 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<vector<pair<int, double>>> g(n);
+        for (int i = 0; i < edges.size(); ++i) {
+            int a = edges[i][0], b = edges[i][1];
+            double s = succProb[i];
+            g[a].push_back({b, s});
+            g[b].push_back({a, s});
+        }
+        vector<double> d(n);
+        d[start_node] = 1.0;
+        queue<pair<double, int>> q;
+        q.push({1.0, start_node});
+        while(!q.empty()){
+            auto a= q.front();
+            q.pop();
+            double w= a.first;
+            int u=a.second;
+            if(d[u]>w){
+                continue;
+            }
+            for(auto &e:g[u]){
+                int v = e.first;
+                double w=  e.second;
+                if (d[v] < d[u] * w) {
+                    d[v] = d[u] * w;
+                    q.push({d[v], v});
+                }
+            }
+        }
+        return d[end_node];
+    }
+};
