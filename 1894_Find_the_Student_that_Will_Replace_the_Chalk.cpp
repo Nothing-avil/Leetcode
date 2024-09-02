e50ed9e16f05766c6c8e766385d69d31b6a1e039
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-02 22:52:14 +0530

    Create 1894_Find_the_Student_that_Will_Replace_the_Chalk.cpp

---
+++ 1894_Find_the_Student_that_Will_Replace_the_Chalk.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long sum= accumulate(chalk.begin(), chalk.end(), 0LL);
+        k = k%sum;
+        for(int i=0; i<chalk.size(); i++){
+            if(k<chalk[i]){
+                return i;
+            }
+            k-=chalk[i];
+        }
+        return -1;
+    }
+};
