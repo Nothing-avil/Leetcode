70b0e03faeed831d96ddfcc67680d8bb8dac387f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-13 22:44:20 +0530

    Create 1310_XOR_Queries_of_a_Subarray.cpp

---
+++ 1310_XOR_Queries_of_a_Subarray.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        for(int i=1; i<arr.size(); i++){
+            arr[i] = arr[i]^arr[i-1];
+        }
+        vector<int> answer(queries.size());
+        for(int q = 0; q< queries.size(); q++){
+            int l = queries[q][0];
+            int r = queries[q][1];
+            if(l>0){
+                answer[q] = arr[r]^arr[l-1];
+            }
+            else{
+                answer[q] = arr[r];
+            }
+        }
+        return answer;
+    }
+};
