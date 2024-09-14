e9a51fb2c0e1a86b37bc5297661fa8d220752e6a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-14 22:59:22 +0530

    Create 2419_Longest_Subarray_With_Maximum_Bitwise_AND.cpp

---
+++ 2419_Longest_Subarray_With_Maximum_Bitwise_AND.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxi = *max_element(nums.begin(), nums.end());
+        int max_sub=0;
+        int c=0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == maxi && nums[i-1] == maxi){
+                c++;
+            }
+            else{
+                c=0;
+            }
+            max_sub = max(max_sub, c);
+        }
+        return max_sub+1;
+    }
+};
