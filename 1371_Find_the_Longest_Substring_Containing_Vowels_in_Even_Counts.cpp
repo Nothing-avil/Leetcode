235b7235c06ca597147d854ec9befbf5cbdcb217
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-15 23:37:30 +0530

    Create 1371_Find_the_Longest_Substring_Containing_Vowels_in_Even_Counts.cpp

---
+++ 1371_Find_the_Longest_Substring_Containing_Vowels_in_Even_Counts.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        vector<int> pos(32, INT_MAX);
+        pos[0] = -1;
+        string vowels = "aeiou";
+        int state = 0, ans = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            for (int j = 0; j < 5; ++j)
+                if (s[i] == vowels[j])
+                    state ^= (1 << j);
+            ans = max(ans, i - pos[state]);
+            pos[state] = min(pos[state], i);
+        }
+        return ans;
+    }
+};
