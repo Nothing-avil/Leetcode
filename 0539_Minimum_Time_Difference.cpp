c32f6837eac742488e6fd56773d2c5de8e31b272
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-16 23:11:10 +0530

    Create 0539_Minimum_Time_Difference.cpp

---
+++ 0539_Minimum_Time_Difference.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        vector<int> int_time;
+        for( auto i : timePoints){
+            int hr = stoi(i.substr(0, 2));
+            int mn = stoi(i.substr(3));
+            int_time.push_back((hr*60 + mn));
+        }
+        sort(int_time.begin(), int_time.end());
+        int n = int_time.size();
+        int mid_ni = 1440;
+        int diff = abs(int_time[n-1]-mid_ni) + int_time[0];
+        for(int i=0; i<n-1; i++){
+            diff = min(diff, abs(int_time[i] - int_time[i+1]));
+        }
+        return diff;
+    }
+};
