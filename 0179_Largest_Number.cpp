123339fda1224f8c2a7ce0491849d8a997ac8092
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-18 22:55:03 +0530

    Create 0179_Largest_Number.cpp

---
+++ 0179_Largest_Number.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> pq;
+        for(int i : nums){
+            pq.push_back(to_string(i));
+        }
+        sort(pq.begin(), pq.end(), [](string a, string b){
+            return a+b > b+a;
+        });
+        if(pq[0] == "0"){
+            return "0";
+        }
+        string answer = "";
+        int i=0;
+        while(i < pq.size()){
+            answer += pq[i++];
+        }
+        return answer;
+    }
+};
