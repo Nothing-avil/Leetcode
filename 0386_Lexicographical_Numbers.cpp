059eed43c9cceabdd3d99cdf0680398815fc138f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-21 23:41:25 +0530

    Create 0386_Lexicographical_Numbers.cpp

---
+++ 0386_Lexicographical_Numbers.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        function<void(int)> dfs = [&](int s){
+            if(s>n){
+                return;
+            }
+            res.push_back(s);
+            for(int i=0; i<=9; i++){
+                dfs((s*10+i));
+            }
+        };
+        for(int i=1; i<=9; i++){
+            dfs(i);
+        }
+        return res;
+    }
+};
