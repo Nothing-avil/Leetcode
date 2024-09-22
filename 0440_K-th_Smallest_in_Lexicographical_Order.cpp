4fbba6f2fd913abdbbc39a917325bc080057f70b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-22 23:21:36 +0530

    Create 0440_K-th_Smallest_in_Lexicographical_Order.cpp

---
+++ 0440_K-th_Smallest_in_Lexicographical_Order.cpp
@@ -0,0 +1,46 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int n;
+    int findKthNumber(int n, int k) {
+        this->n = n;
+        --k;
+        long long curr = 1;
+        while (k) {
+            int cnt = count(curr);
+            if (k >= cnt) {
+                k -= cnt;
+                ++curr;
+            } else {
+                --k;
+                curr *= 10;
+            }
+        }
+        return (int) curr;
+    }
+
+    int count(long long curr) {
+        long long next = curr + 1;
+        int cnt = 0;
+        while (curr <= n) {
+            cnt += min(n - curr + 1, next - curr);
+            next *= 10;
+            curr *= 10;
+        }
+        return cnt;
+    }
+};
