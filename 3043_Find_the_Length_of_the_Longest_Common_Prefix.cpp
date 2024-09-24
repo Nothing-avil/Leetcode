a5c3738de9148ff161e542712b812b8813004608
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-24 22:15:44 +0530

    Create 3043_Find_the_Length_of_the_Longest_Common_Prefix.cpp

---
+++ 3043_Find_the_Length_of_the_Longest_Common_Prefix.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        vector<pair<string, int>> all;
+        for(int v : arr1){
+            all.emplace_back(to_string(v), 0);
+        }
+        for(int v : arr2){
+            all.emplace_back(to_string(v), 1);
+        }
+        sort(all.begin(), all.end());
+        vector<string> last(2);
+        int ans = 0;
+        for(auto [k, v] : all){
+            string u = last[v ^ 1];
+            int p = 0;
+            while(p < u.size() && p < k.size() && u[p] == k[p]) p++;
+            ans = max(ans, p);
+            last[v] = k;
+        }
+        return ans;
+    }
+};
