8d33376548d9b985e28b67b501c76980cd9cdc23
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-27 20:23:40 +0530

    Create 0731_My_Calendar_II.cpp

---
+++ 0731_My_Calendar_II.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class MyCalendarTwo {
+    vector<pair<int,int>> b;
+    vector<pair<int,int>>db;
+public:
+    MyCalendarTwo() {}
+    bool book(int start, int end) {
+        for(pair<int,int> x: db){
+            if(start<x.second && end>x.first) return false;
+        }
+        for(pair<int,int> x : b){
+            if(start<x.second && end>x.first){
+                db.push_back({max(start,x.first),min(end,x.second)});
+            }
+        }
+        b.push_back({start,end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+*/
