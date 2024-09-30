ff322df567f11d9909d5a5faccd5b8f91eb35248
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-30 23:55:33 +0530

    Create 1381_Design_a_Stack_With_Increment_Operation.cpp

---
+++ 1381_Design_a_Stack_With_Increment_Operation.cpp
@@ -0,0 +1,60 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class CustomStack {
+public:
+    CustomStack(int maxSize) {
+        stk.resize(maxSize);
+        add.resize(maxSize);
+        i = 0;
+        
+    }
+
+    void push(int x) {
+        if (i < stk.size()) {
+            stk[i++] = x;
+        }
+    }
+
+    int pop() {
+        if (i <= 0) {
+            return -1;
+        }
+        int ans = stk[--i] + add[i];
+        if (i > 0) {
+            add[i - 1] += add[i];
+        }
+        add[i] = 0;
+        return ans;
+    }
+
+    void increment(int k, int val) {
+        if (i > 0) {
+            add[min(k, i) - 1] += val;
+        }
+    }
+
+private:
+    vector<int> stk;
+    vector<int> add;
+    int i;
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
