6d9421051f5ebcd30dad5cb51364b7b631cc94b9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-02 00:00:10 +0530

    Create 1497_Check_If_Array_Pairs_Are_Divisible_by_k.cpp

---
+++ 1497_Check_If_Array_Pairs_Are_Divisible_by_k.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> freq(k);
+        for(int i : arr){
+            freq[((i%k)+k)%k]++;
+        }
+        for(int i=1; i<k; i++){
+            if(freq[i] != freq[k-i]){
+                return false;
+            }
+        }
+        return freq[0]%2==0;
+    }
+};
