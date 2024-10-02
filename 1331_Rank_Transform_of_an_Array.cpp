e6fe07a55963174f56fd877e22bea7fca43aab0f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-02 12:23:54 +0530

    Create 1331_Rank_Transform_of_an_Array.cpp

---
+++ 1331_Rank_Transform_of_an_Array.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> che = arr;
+        sort(che.begin(), che.end());
+        unordered_map<int, int> mp;
+        for(int i=0, j=1; i<arr.size(); i++){
+            if(!mp.count(che[i])){
+                mp[che[i]]= j++;
+            }
+        }
+        for(int i=0; i<arr.size(); i++){
+            arr[i] = mp[arr[i]];
+        }
+        return arr;
+    }
+};
