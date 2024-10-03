ce87a47b5445fb8e7e56c051c5129d5240c38752
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-03 23:57:04 +0530

    Create 1590_Make_Sum_Divisible_by_P.cpp

---
+++ 1590_Make_Sum_Divisible_by_P.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int sum =0;
+        for(int i=0; i<nums.size(); i++){
+            sum = (sum+nums[i])%p;
+        }
+        int tar = sum;
+        if(tar==0){
+            return 0;
+        }
+        unordered_map<int, int> mp;
+        int res =0, ans= INT_MAX;
+        mp[0] = -1;
+        for(int i=0; i<nums.size(); i++){
+            res = (res + nums[i])%p;
+            int re = (res - tar + p)%p;
+            if(mp.count(re)){
+                ans = min(ans, i - mp[re]);
+            }
+            mp[res] = i;
+        }
+        return ans == nums.size() ? -1 : ans;
+    }
+};
