37ed0386f5eebcfbc987a2b356169f28fec831da
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-04 23:35:39 +0530

    Create 2491_Divide_Players_Into_Teams_of_Equal_Skill.cpp

---
+++ 2491_Divide_Players_Into_Teams_of_Equal_Skill.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(), skill.end());
+        long long sums = accumulate(skill.begin(), skill.end(), 0ll);
+        long long target = sums/(skill.size()/2);
+        int i=0, j=skill.size()-1;
+        long long total=0;
+        while(i<j){
+            if(target != (skill[i] + skill[j])){
+                return -1;
+            }
+            total += skill[i]*skill[j];
+            i++;
+            j--;
+        }
+        return total;
+    }
+};
