9650da24257ccd94ce02f4074f81f52e0eb8af2b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-08 23:40:10 +0530

    Create 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp

---
+++ 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
@@ -0,0 +1,47 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minSwaps(string s) {
+
+        //APPROCH 1
+        stack<char> st;
+        for(char ch : s){
+            if(st.empty()){
+                st.push(ch);
+                continue;
+            }
+            char cc = st.top();
+            if(cc == '[' && ch== ']'){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        return (ceil((double) st.size()/4.0));
+        
+        //APPROACH 2
+        int x = 0;
+        for (char& c : s) {
+            if (c == '[') {
+                ++x;
+            } else if (x) {
+                --x;
+            }
+        }
+        return (x + 1) / 2;
+    }
+};
