cd10803f20f0516a6f5cc2bb27339455336e666b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-09 19:28:08 +0530

    Create 0921_Minimum_Add_to_Make_Parentheses_Valid.cpp

---
+++ 0921_Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,47 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+      //APPROCH 1
+      stack<char> st;
+      for(char ch : s){
+          if(st.empty()){
+              st.push(ch);
+              continue;
+          }
+          char cc = st.top();
+          if(ch == ')' && cc == '('){
+              st.pop();
+          }
+          else{
+              st.push(ch);
+          }
+      }
+      return st.size();
+      
+      //APPROCH 2
+        int c=0, d=0;
+        for(char ch : s){
+            if(ch == '('){
+                c++;
+            }
+            else{
+                c>0 ? c-- : d++;
+            }
+        }
+        return c+d;
+    }
+};
