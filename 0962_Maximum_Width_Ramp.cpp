3049c17e59153584eacb0da9630b46c49a0932de
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-10 22:53:03 +0530

    Create 0962_Maximum_Width_Ramp.cpp

---
+++ 0962_Maximum_Width_Ramp.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            if (stk.empty() || nums[stk.top()] > nums[i])
+                stk.push(i);
+        }
+        int ans = 0;
+        for (int i = n - 1; i; --i) {
+            while (!stk.empty() && nums[stk.top()] <= nums[i]) {
+                ans = max(ans, i - stk.top());
+                stk.pop();
+            }
+            if (stk.empty())
+                break;
+        }
+        return ans;
+    }
+};
