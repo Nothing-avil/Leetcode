4b4cdf0111771e02e6099e2608e9fbdd8430dfb3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-17 00:11:27 +0530

    Create 1405_Longest_Happy_String.cpp

---
+++ 1405_Longest_Happy_String.cpp
@@ -0,0 +1,49 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        using pci = pair<char, int>;
+        auto cmp = [](pci x, pci y) { return x.second < y.second; };
+        priority_queue<pci, vector<pci>, decltype(cmp)> pq(cmp);
+
+        if (a > 0) pq.push({'a', a});
+        if (b > 0) pq.push({'b', b});
+        if (c > 0) pq.push({'c', c});
+
+        string ans;
+        while (!pq.empty()) {
+            pci cur = pq.top();
+            pq.pop();
+            int n = ans.size();
+            if (n >= 2 && ans[n - 1] == cur.first && ans[n - 2] == cur.first) {
+                if (pq.empty()) break;
+                pci nxt = pq.top();
+                pq.pop();
+                ans.push_back(nxt.first);
+                if (--nxt.second > 0) {
+                    pq.push(nxt);
+                }
+                pq.push(cur);
+            } else {
+                ans.push_back(cur.first);
+                if (--cur.second > 0) {
+                    pq.push(cur);
+                }
+            }
+        }
+        return ans;
+    }
+};
