05d1350ddca374e2b9063409d69ba12dcb998bdd
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-17 18:24:29 +0530

    Create 0670_Maximum_Swap.cpp

---
+++ 0670_Maximum_Swap.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        int n = s.size();
+        vector<int> d(n);
+        iota(d.begin(), d.end(), 0);
+        for (int i = n - 2; ~i; --i) {
+            if (s[i] <= s[d[i + 1]]) {
+                d[i] = d[i + 1];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int j = d[i];
+            if (s[i] < s[j]) {
+                swap(s[i], s[j]);
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
