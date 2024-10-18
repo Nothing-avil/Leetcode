beae299f0681a79743791e00d281856292871694
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-19 00:18:49 +0530

    Create 2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp

---
+++ 2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int mx;
+    int ans;
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        dfs(0, 0, nums);
+        return ans;
+    }
+
+    void dfs(int u, int t, vector<int>& nums) {
+        if (u == nums.size()) {
+            if (t > mx) {
+                mx = t;
+                ans = 1;
+            } else if (t == mx)
+                ++ans;
+            return;
+        }
+        dfs(u + 1, t, nums);
+        dfs(u + 1, t | nums[u], nums);
+    }
+};
