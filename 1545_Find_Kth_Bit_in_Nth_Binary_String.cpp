9d39d9859675399e91e54e9a746212a989ef9568
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-19 22:55:15 +0530

    Create 1545_Find_Kth_Bit_in_Nth_Binary_String.cpp

---
+++ 1545_Find_Kth_Bit_in_Nth_Binary_String.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if(k==1){
+            return '0';
+        }
+        string str= "0";
+        while(--n){
+            string s= str;
+            int mid = s.size()/2;
+            if (s[mid] == '0') {
+                s[mid] = '1';
+            } else {
+                s[mid] = '0';
+            }
+            str = str + '1' + s;
+        }
+        return str[k-1];
+    }
+};
