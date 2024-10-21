80594f7029372580edf6db6e3dbf05db7f353dbe
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-21 21:39:31 +0530

    Create 1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.cpp

---
+++ 1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> st;
+        int ans = 1;
+        function<void(int, int)> ddfs = [&](int j, int t){
+            if(j >= s.size()){
+                ans = max(t, ans);
+                return;
+            }
+            for(int i = j+1; i<=s.size(); i++){
+                string x = s.substr(j, i-j);
+                if(!st.count(x)){
+                    st.insert(x);
+                    ddfs(i, t+1);
+                    st.erase(x);
+                }
+            }
+        };
+        ddfs(0, 0);
+        return ans;
+    }
+};
