8cd08e23a3868a017806a162a7fd88ff2c32078c
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-22 22:19:53 +0530

    Create 2583_Kth_Largest_Sum_in_a_Binary_Tree.cpp

---
+++ 2583_Kth_Largest_Sum_in_a_Binary_Tree.cpp
@@ -0,0 +1,53 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        queue<TreeNode*> qu;
+        priority_queue<long long , vector<long long >, greater<long long >> pt;
+        qu.push(root);
+        while(!qu.empty()){
+            int n = qu.size();
+            long long sum=0;
+            while(n--){
+                TreeNode* t = qu.front();
+                qu.pop();
+                sum+= t->val;
+                if(t->left){
+                    qu.push(t->left);
+                }
+                if(t->right){
+                    qu.push(t->right);
+                }
+            }
+            pt.push(sum);
+            if(pt.size() > k){
+                pt.pop();
+            }
+        }
+        return (k>pt.size()) ? -1: pt.top();
+    }
+};
