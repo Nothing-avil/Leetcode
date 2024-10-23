1884d3f511020d07adbed15cb7350430b9f1d61f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-23 23:53:01 +0530

    Create 2641_Cousins_in_Binary_Tree_II.cpp

---
+++ 2641_Cousins_in_Binary_Tree_II.cpp
@@ -0,0 +1,57 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        root->val = 0;
+        vector<TreeNode*> q = {root};
+        while (!q.empty()) {
+            vector<TreeNode*> t;
+            int s = 0;
+            for (TreeNode* node : q) {
+                if (node->left) {
+                    t.emplace_back(node->left);
+                    s += node->left->val;
+                }
+                if (node->right) {
+                    t.emplace_back(node->right);
+                    s += node->right->val;
+                }
+            }
+            for (TreeNode* node : q) {
+                int sub = (node->left ? node->left->val : 0) + (node->right ? node->right->val : 0);
+                if (node->left) {
+                    node->left->val = s - sub;
+                }
+                if (node->right) {
+                    node->right->val = s - sub;
+                }
+            }
+            q = move(t);
+        }
+        return root;
+    }
+};
