462774a499b347a640e07de92a96ac7867b670ed
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-24 21:49:17 +0530

    Create 0951_Flip_Equivalent_Binary_Trees.cpp

---
+++ 0951_Flip_Equivalent_Binary_Trees.cpp
@@ -0,0 +1,42 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool solver(TreeNode* root1, TreeNode* root2){
+        if(root1 == root2 || (!root1 && !root2)){
+            return true;
+        }
+        if(!root1 || !root2 || root1->val != root2->val){
+            return false;
+        }
+        return ((solver(root1->left, root2->left) && solver(root1->right, root2->right)) || (solver(root1->left, root2->right) && solver(root1->right, root2->left)));
+    }
+
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        return solver(root1, root2);
+    }
+};
