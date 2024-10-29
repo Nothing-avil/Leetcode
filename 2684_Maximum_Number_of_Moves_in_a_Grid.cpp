3b9ee0877e43ac6a4822d61baf7c400e689ed377
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-30 00:20:23 +0530

    Create 2684_Maximum_Number_of_Moves_in_a_Grid.cpp

---
+++ 2684_Maximum_Number_of_Moves_in_a_Grid.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        unordered_set<int> a, b;
+        for (int i = 0; i < n; i++) {
+            a.insert(i);
+        }
+        for (int j = 0; j < m - 1; ++j) {
+            b.clear();
+            for (int i : a) {
+                for (int k = i - 1; k <= i + 1; ++k) {
+                    if (k >= 0 && k < n && grid[i][j] < grid[k][j + 1]) {
+                        b.insert(k);
+                    }
+                }
+            }
+            if (b.empty()) {
+                return j;
+            }
+            a.swap(b);
+        }
+        return m - 1;
+    }
+};
