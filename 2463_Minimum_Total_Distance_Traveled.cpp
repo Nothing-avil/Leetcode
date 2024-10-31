5d26c450fa78446f66f902e24d24f1f4370329f0
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-01 00:23:22 +0530

    Create 2463_Minimum_Total_Distance_Traveled.cpp

---
+++ 2463_Minimum_Total_Distance_Traveled.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+        vector<vector<long long>> f(robot.size(), vector<long long>(factory.size()));
+        function<long long(int i, int j)> dfs = [&](int i, int j) -> long long {
+            if (i == robot.size()) return 0;
+            if (j == factory.size()) return 1e15;
+            if (f[i][j]) return f[i][j];
+            long long ans = dfs(i, j + 1);
+            long long t = 0;
+            for (int k = 0; k < factory[j][1]; ++k) {
+                if (i + k >= robot.size()) break;
+                t += abs(robot[i + k] - factory[j][0]);
+                ans = min(ans, t + dfs(i + k + 1, j + 1));
+            }
+            f[i][j] = ans;
+            return ans;
+        };
+        return dfs(0, 0);
+    }
+};
