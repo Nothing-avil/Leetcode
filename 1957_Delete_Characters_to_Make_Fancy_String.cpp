ec1f94a706abe3f3ac1a0e94026c4f1f9acd2073
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-01 19:26:44 +0530

    Create 1957_Delete_Characters_to_Make_Fancy_String.cpp

---
+++ 1957_Delete_Characters_to_Make_Fancy_String.cpp
@@ -0,0 +1,32 @@
+
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int j=2;
+        int n=s.size();
+        if(n<=2){
+            return s;
+        }
+        for(int k=2; k<n; k++){
+            if(s[j-1]!=s[k] || s[j-2]!= s[k]){
+                s[j++] = s[k];
+            }
+        }
+        s.resize(j);
+        return s;
+    }
+};
