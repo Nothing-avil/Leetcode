039389884329cd9abf824108ec1a69877bdf9807
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-04 23:28:53 +0530

    Create 3163_String_Compression_III.cpp

---
+++ 3163_String_Compression_III.cpp
@@ -0,0 +1,37 @@
+
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string compressedString(string word) {
+        string ans;
+        int n = word.length();
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            while (j < n && word[j] == word[i]) {
+                ++j;
+            }
+            int k = j - i;
+            while (k > 0) {
+                int x = min(9, k);
+                ans.push_back('0' + x);
+                ans.push_back(word[i]);
+                k -= x;
+            }
+            i = j;
+        }
+        return ans;
+    }
+};
