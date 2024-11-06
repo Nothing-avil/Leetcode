a658ebf88d712a3e6b63afa73afef9b029e38106
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-06 19:30:37 +0530

    Create 3011_Find_if_Array_Can_Be_Sorted.cpp

---
+++ 3011_Find_if_Array_Can_Be_Sorted.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        bool check = true;
+        do{
+            check = false;
+            for(int i=1; i<nums.size(); i++){
+                if(__builtin_popcount(nums[i]) == __builtin_popcount(nums[i-1])){
+                    if(nums[i-1] > nums[i]){
+                        swap(nums[i], nums[i-1]);
+                        check = true;
+                    }
+                }
+            }
+        }while(check);
+        return is_sorted(nums.begin(), nums.end());
+    }
+};
