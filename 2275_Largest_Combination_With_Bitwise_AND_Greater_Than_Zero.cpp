fa9d67fec6447b1c7ba2ed928fe8afd025d1eb8a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-07 23:32:57 +0530

    Create 2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.cpp

---
+++ 2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int mx = *max_element(candidates.begin(), candidates.end());
+        int m = 32 - __builtin_clz(mx);
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            int cnt = 0;
+            for (int x : candidates) {
+                cnt += x >> i & 1;
+            }
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
