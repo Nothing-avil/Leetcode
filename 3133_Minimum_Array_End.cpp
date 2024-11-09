470411a62cf4e4c74b3c249f2ec5011859047b7f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-09 19:40:12 +0530

    Create 3133_Minimum_Array_End.cpp

---
+++ 3133_Minimum_Array_End.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        vector<long long> res;
+        for(long long i=0; i<32; i++){
+            if(!((1<<i)&x)){
+                res.push_back(i);
+            }
+        }
+        for(long long i=32; i<64; i++){
+            res.push_back(i);
+        }
+        long long c=x;
+        long long cnt = n-1;
+        for(int i=0; i<res.size(); i++, cnt>>=1){
+            c += (cnt & 1) << res[i];
+        }
+        return c;
+    }
+};
