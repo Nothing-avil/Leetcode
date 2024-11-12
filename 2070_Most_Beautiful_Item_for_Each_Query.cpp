377149bd564ddfdc602bf7e91c56d5d4506ccd22
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-13 00:03:48 +0530

    Create 2070_Most_Beautiful_Item_for_Each_Query.cpp

---
+++ 2070_Most_Beautiful_Item_for_Each_Query.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end());
+        int n = items.size();
+        int m = queries.size();
+        vector<int> prices(n, items[0][0]);
+        for (int i = 1; i < n; ++i) {
+            prices[i] = items[i][0];
+            items[i][1] = max(items[i - 1][1], items[i][1]);
+        }
+        vector<int> ans;
+        for (int q : queries) {
+            int j = upper_bound(prices.begin(), prices.end(), q) - prices.begin() - 1;
+            ans.push_back(j < 0 ? 0 : items[j][1]);
+        }
+        return ans;
+    }
+};
