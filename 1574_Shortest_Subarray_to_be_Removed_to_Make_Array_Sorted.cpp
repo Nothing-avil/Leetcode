f8f98b1f1fc31d816156e381c4fd977cf51e97f8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-15 23:55:24 +0530

    Create 1574_Shortest_Subarray_to_be_Removed_to_Make_Array_Sorted.cpp

---
+++ 1574_Shortest_Subarray_to_be_Removed_to_Make_Array_Sorted.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int j= n-1;
+        while(j>0 && arr[j] >= arr[j-1]){
+            j--;
+        }
+        int i=0; 
+        int ch = j;
+        while(i<j && (i==0 || arr[i] >= arr[i-1])){
+            while(j < n && arr[i] > arr[j]){
+                j++;
+            }
+            ch= min(ch, j-i-1);
+            i++;
+        }
+        return ch;
+    }
+};
