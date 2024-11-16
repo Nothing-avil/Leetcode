3fd2b21b68ad27fd050d989c5fd01ecff43d3031
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-16 23:25:01 +0530

    Create 3254_Find_the_Power_of_K-Size_Subarrays_I.cpp

---
+++ 3254_Find_the_Power_of_K-Size_Subarrays_I.cpp
@@ -0,0 +1,41 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        vector<int> res(nums.size()-k+1);
+        function<bool(int, int)> check = [&](int i, int  j){
+            while(i<j){
+                if(nums[i]+1 != nums[i+1]){
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        };
+        int i=0, j=k-1;
+        while(j<nums.size()){
+            if(check(i, j)){
+                res[i] = nums[j];
+            }
+            else{
+                res[i] = -1;
+            }
+            i++;
+            j++;
+        }
+        return res;
+    }
+};
