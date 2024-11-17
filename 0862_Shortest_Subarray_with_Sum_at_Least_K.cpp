0055b4589e15b993b4697b77cf49f657946e82a7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-17 23:45:01 +0530

    Create 0862_Shortest_Subarray_with_Sum_at_Least_K.cpp

---
+++ 0862_Shortest_Subarray_with_Sum_at_Least_K.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long> pre(n+1);
+        for(int i=0; i<n; i++){
+            pre[i+1] = pre[i] + nums[i];
+        }
+        deque<int> dq;
+        int res = n+1;
+        for(int i=0; i<=n; i++){
+            while(!dq.empty() && pre[i] - pre[dq.front()] >= k){
+                res = min(res, i-dq.front());
+                dq.pop_front();
+            }
+            while(!dq.empty() && pre[dq.back()] >= pre[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        return res > n ? -1 : res;
+    }
+};
