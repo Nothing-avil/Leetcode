afd5f73e5a2a849709d0983fb99ce9339b431470
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-18 23:29:08 +0530

    Create 1652_Defuse_the_Bomb.cpp

---
+++ 1652_Defuse_the_Bomb.cpp
@@ -0,0 +1,42 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> res(n, 0);
+        if(k==0){
+            return res;
+        }
+        function<int(int)> solver = [&](int a){
+            int sum = 0;
+            if(k>0){
+                for(int i=1; i<=k; i++){
+                    sum += code[(a+i)%n];
+                }
+            }
+            else{
+                for(int i=1; i<=abs(k); i++){
+                    sum += code[((n-1) + a + 1 - i)%n];
+                }
+            }
+            return sum;
+        };
+        for(int i=0; i<n; i++){
+            res[i] = solver(i);
+        }
+        return res;
+    }
+};
