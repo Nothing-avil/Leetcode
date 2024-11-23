31abe9a159b8873b7c880be9bc9fa96038e41f77
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-24 00:32:02 +0530

    Create 1861_Rotating_the_Box.cpp

---
+++ 1861_Rotating_the_Box.cpp
@@ -0,0 +1,49 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size();
+        int n = box[0].size();
+        vector<vector<char>> res(n, vector<char>(m));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                res[j][m-i-1] = box[i][j];
+            }
+        }
+        for(int i=0; i<m; i++){
+            queue<int> qu;
+            for(int j= n-1; ~j; j--){
+                if(res[j][i] == '*'){
+                    //remove;
+                    queue<int> t;
+                    swap(t, qu);
+                }
+                else if(res[j][i] == '.'){
+                    //space
+                    qu.push(j);
+                }
+                else if(!qu.empty()){
+                    //stone put
+                    res[qu.front()][i] = '#';
+                    qu.pop();
+                    res[j][i] = '.';
+                    qu.push(j);
+                }
+            }
+        }
+        return res;
+    }
+};
