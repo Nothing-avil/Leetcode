eee108711614ad2546ca832f133cca0cd9b10e89
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-11-27 00:05:20 +0530

    Create 2924_Find_Champion_II.cpp

---
+++ 2924_Find_Champion_II.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        int st[n];
+        memset(st, 0, sizeof(st));
+        for(auto i : edges){
+            ++st[i[1]];
+        }
+        int c = 0;
+        int s = 0;
+        for(int i=0;i<n; i++){
+            if(st[i] == 0){
+                s=i;
+                c++;
+            }
+        }
+        return c==1 ? s : -1;
+    }
+};
