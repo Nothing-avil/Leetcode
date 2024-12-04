e80f6a777971dc71ab10290ef527e2f928f7b380
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-04 12:08:49 +0530

    Create 2825_Make_String_a_Subsequence_Using_Cyclic_Increments.cpp

---
+++ 2825_Make_String_a_Subsequence_Using_Cyclic_Increments.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int n= str1.size(), m = str2.size();
+        int i=0, j=0;
+        while(i<n && j< m){
+            int c = str1[i] == 'z' ? 'a' : str1[i] +1;
+            if(str2[j] == c || str2[j] - str1[i] == 0){
+                j++;
+            }
+            i++;
+        }
+        if(j==m){
+            return true;
+        }
+        return false;
+    }
+};
