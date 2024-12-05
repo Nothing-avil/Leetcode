c001b6ff6255f2e6cbeed2d606effe123e2e65ab
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-05 23:28:35 +0530

    Create 2337_Move_Pieces_to_Obtain_a_String.cpp

---
+++ 2337_Move_Pieces_to_Obtain_a_String.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int i=0, j=0;
+        int n1= start.size(), n2= target.size();
+        while(true){
+            while (i < n1 && start[i] == '_') ++i;
+            while (j < n2 && target[j] == '_') ++j;
+            if(i==n1 && j==n2){
+                return true;
+            }
+            if(i==n1 || j==n2 || start[i] != target[j]){
+                return false;
+            }
+            if(start[i] == 'L' && i < j){
+                return false;
+            }
+            if(start[i]== 'R' && i > j){
+                return false;
+            }
+            i++;
+            j++;
+        }
+    }
+};
