b11f8cb1ed564e19ca50894b8a8e03e65dadcb29
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-06 19:52:39 +0530

    Create 2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.cpp

---
+++ 2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        vector<int> dp(1e4+1, 1);
+        for(int i : banned){
+            dp[i] = 0;
+        }
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            if(dp[i] == 0){
+                continue;
+            }
+            if(maxSum >= i){
+                ans++;
+                maxSum-=i;
+                dp[i] = 0;
+            }
+        }
+        return ans;
+    }
+};
