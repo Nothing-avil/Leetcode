4bd926621e09fe5a5eef6441c27aac5d4a1fc4da
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-07 16:48:24 +0530

    Create 1760_Minimum_Limit_of_Balls_in_a_Bag.cpp

---
+++ 1760_Minimum_Limit_of_Balls_in_a_Bag.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int s =1, e = *max_element(nums.begin(), nums.end());
+        while(s<e){
+            int mid = (s+e)>>1;
+            long long cnt = 0;
+            for (int x : nums) {
+                cnt += (x - 1) / mid;
+            }
+            if(cnt <= maxOperations){
+                e = mid;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return s;
+    }
+};
