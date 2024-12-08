a33716d3561b7e468963ff568c1c255bf701e2bd
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-08 23:07:19 +0530

    Create 2054_Two_Best_Non-Overlapping_Events.cpp

---
+++ 2054_Two_Best_Non-Overlapping_Events.cpp
@@ -0,0 +1,44 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        int n= events.size();
+        sort(events.begin(), events.end());
+        vector<int> dp(n+1);
+        for(int i=n-1; i; i--){
+            dp[i] = max(dp[i+1], events[i][2]);
+        }
+        int ans = 0;
+        for(auto& e : events){
+            int a = e[2];
+            int s=0, end = n;
+            while(s < end){
+                int mid= (s+end)>>1;
+                if(events[mid][0] > e[1]){
+                    end = mid;
+                }
+                else{
+                    s = mid+1;
+                }
+            }
+            if(s < n){
+                a += dp[s];
+            }
+            ans = max(ans, a);
+        }
+        return ans;
+    }
+};
