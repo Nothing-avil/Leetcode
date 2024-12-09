a2980cec47024a15cc9b86c3d77d9c708d1fee38
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-10 00:58:43 +0530

    Create 3152_Special_Array_II.cpp

---
+++ 3152_Special_Array_II.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<bool> res;
+        int n=nums.size();
+        vector<int> party(n);
+        for(int i=1; i<n; i++){
+            party[i]= party[i-1] + ((nums[i]%2)==(nums[i-1]%2)?1:0);
+        }
+        for(auto i: queries){
+            int f= i[0];
+            int t= i[1];
+            int s= party[t]-party[f];
+            res.push_back((s==0));
+        }
+        return res;
+    }
+};
