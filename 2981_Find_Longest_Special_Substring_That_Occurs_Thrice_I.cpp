5d3675f8136b529c82801bfe376993c0890d03c4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-10 23:13:31 +0530

    Create 2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.cpp

---
+++ 2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.cpp
@@ -0,0 +1,46 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.size();
+        int l = 0, r = n;
+        auto check = [&](int x) {
+            int cnt[26] = {};
+            for (int i = 0; i < n;) {
+                int j = i + 1;
+                while (j < n && s[j] == s[i]) {
+                    ++j;
+                }
+                int k = s[i] - 'a';
+                cnt[k] += max(0, j - i - x + 1);
+                if (cnt[k] >= 3) {
+                    return true;
+                }
+                i = j;
+            }
+            return false;
+        };
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (check(mid)) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l == 0 ? -1 : l;
+    }
+};
