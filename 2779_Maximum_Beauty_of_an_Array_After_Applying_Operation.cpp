3c8cdee10f4827f458fbad1c2255fc24c5e9d5f2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-11 23:20:14 +0530

    Create 2779_Maximum_Beauty_of_an_Array_After_Applying_Operation.cpp

---
+++ 2779_Maximum_Beauty_of_an_Array_After_Applying_Operation.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int maxi = *max_element(nums.begin(), nums.end()) + k*2+2;
+        vector<int> dp(maxi);
+        for(int i : nums){
+            dp[i]++;
+            dp[i+2*k+1]--;
+        }
+        int ans = 0;
+        int s = 0;
+        for(int i : dp){
+            s+=i;
+            ans = max(ans, s);
+        }
+        return ans;
+    }
+};
