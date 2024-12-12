2c7c09df200ded8ce768dfea0ac94569cf8a9056
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-12 22:26:34 +0530

    Create 2558_Take_Gifts_From_the_Richest_Pile.cpp

---
+++ 2558_Take_Gifts_From_the_Richest_Pile.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+        for(int i : gifts){
+            pq.push(i);
+        }
+        while(k--){
+            int k = pq.top();
+            pq.pop();
+            int x = sqrt(k);
+            cout << x << endl;
+            pq.push(x);
+        }
+        long long answer = 0;
+        while(pq.size()){
+            int a = pq.top();
+            pq.pop();
+            answer += a;
+        }
+        return answer;
+    }
+};
