3cbf0bca46748a62010fb51c96bbb5400584d259
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-16 11:25:42 +0530

    Create 3264_Final_Array_State_After_K_Multiplication_Operations_I.cpp

---
+++ 3264_Final_Array_State_After_K_Multiplication_Operations_I.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        for(int i=0; i<nums.size(); i++){
+            pq.push({nums[i], i});
+        }
+        while(k--){
+            int m = pq.top().first;
+            int i = pq.top().second;
+            pq.pop();
+            m*=multiplier;
+            pq.push({m,i});
+        }
+        while(pq.size()){
+            int m = pq.top().first;
+            int i = pq.top().second;
+            pq.pop();
+            nums[i] = m;
+        }
+        return nums;
+    }
+};
