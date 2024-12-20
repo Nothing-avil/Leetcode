717ac36984d7d172149967cc9eb8a90cb356d5cf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-20 22:02:45 +0530

    Create 2415_Reverse_Odd_Levels_of_Binary_Tree.cpp

---
+++ 2415_Reverse_Odd_Levels_of_Binary_Tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        queue<TreeNode*> q{{root}};
+        for (int i = 0; q.size(); ++i) {
+            vector<TreeNode*> t;
+            for (int k = q.size(); k; --k) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (i & 1) {
+                    t.push_back(node);
+                }
+                if (node->left) {
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+            for (int l = 0, r = t.size() - 1; l < r; ++l, --r) {
+                swap(t[l]->val, t[r]->val);
+            }
+        }
+        return root;
+    }
+};
