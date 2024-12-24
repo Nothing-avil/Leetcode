f4406d7346f918f7b065d40f4ff46109725c5441
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-24 22:48:59 +0530

    Create 3203_Find_Minimum_Diameter_After_Merging_Two_Trees.cpp

---
+++ 3203_Find_Minimum_Diameter_After_Merging_Two_Trees.cpp
@@ -0,0 +1,50 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+
+    int treeDiameter(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        vector<int> g[n];
+        for (auto& e : edges) {
+            int a = e[0], b = e[1];
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        int ans = 0, a = 0;
+        auto dfs = [&](this auto&& dfs, int i, int fa, int t) -> void {
+            for (int j : g[i]) {
+                if (j != fa) {
+                    dfs(j, i, t + 1);
+                }
+            }
+            if (ans < t) {
+                ans = t;
+                a = i;
+            }
+        };
+        dfs(0, -1, 0);
+        dfs(a, -1, 0);
+        return ans;
+    }
+
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int d1 = treeDiameter(edges1);
+        int d2 = treeDiameter(edges2);
+
+        cout << d1 << " " << d2;
+        return max({d1, d2, (d1+1)/2 + (d2+1)/2 + 1});
+    }
+};
