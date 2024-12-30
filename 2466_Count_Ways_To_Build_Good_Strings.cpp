a393a5b14155244e6f120cf6f8838c2b179572ce
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-30 22:36:03 +0530

    Create 2466_Count_Ways_To_Build_Good_Strings.cpp

---
+++ 2466_Count_Ways_To_Build_Good_Strings.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        int m=1e9+7;
+        vector<int> dp(200005,0);
+        dp[zero]+=1;
+        dp[one]+=1;
+        for(int i=0;i<=high;i++)
+            dp[i]+=(dp[max(0,i-zero)]+dp[max(0,i-one)])%m;
+        int ans=0;
+        for(int i=low;i<=high;i++)
+            ans=(dp[i]+ans)%m;
+        return ans;
+    }
+};
