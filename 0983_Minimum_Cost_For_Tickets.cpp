41008860a8a3ad3c010e173473efa2655d1134a1
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-12-31 17:12:32 +0530

    Create 0983_Minimum_Cost_For_Tickets.cpp

---
+++ 0983_Minimum_Cost_For_Tickets.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int dp[366] = {0};
+        int n = days.size();
+        int j = 0;
+        for (int i = 1; i <= 365; i++) {
+            if (j >= n) {
+                break;
+            }
+            if (i != days[j]) {
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = dp[i-1] + costs[0];
+                dp[i] = min(dp[i], dp[max(i-7, 0)] + costs[1]);
+                dp[i] = min(dp[i], dp[max(i-30, 0)] + costs[2]); 
+                j++;
+            }
+        }
+        return dp[days.back()];
+    }
+};
