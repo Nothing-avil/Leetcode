ae39d41556ec60b31253696c874bc04533d11ff2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-03 23:38:34 +0530

    Create 2270_Number_of_Ways_to_Split_Array.cpp

---
+++ 2270_Number_of_Ways_to_Split_Array.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        vector<long long> pre(nums.size()), pre2(nums.size());
+        pre[0] = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            pre[i] = pre[i-1] + nums[i];
+        }
+        pre2[nums.size()-1] = nums[nums.size()-1];
+        for(int i=nums.size()-2; i>=0; i--){
+            pre2[i] = pre2[i+1] + nums[i];
+        }
+        int c=0;
+        for(int i=0; i<nums.size()-1; i++){
+            if(pre[i] >= pre2[i+1]){
+                c++;
+            }
+        }
+        return c;
+    }
+};
