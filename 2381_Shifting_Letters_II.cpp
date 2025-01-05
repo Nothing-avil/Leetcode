b0d7be9cc1861fa01d592ab9ee0d14825e7a19e9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-05 14:08:59 +0530

    Create 2381_Shifting_Letters_II.cpp

---
+++ 2381_Shifting_Letters_II.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int> pre(s.size()+1, 0);
+        for(auto it : shifts){
+            int i = it[2];
+            pre[it[0]] += (2*i - 1);
+            pre[it[1]+1] -= (2*i -1);
+        }
+        for(int i=0;i<s.size(); i++){
+            pre[i+1] = (pre[i+1] + pre[i])%26;
+        }
+        pre[0] = pre[0]%26;
+        for(int i=0;i<s.size(); i++){
+            int x=  (26 + (s[i] -'a') + pre[i])%26;
+            s[i] = 'a' + x;
+        }
+        return s;
+    }
+};
