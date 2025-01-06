f88c9d73cfb5e09b1ed2ff442338cd91732fc97b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-06 22:41:41 +0530

    Create 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp

---
+++ 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        vector<int> ans(n);
+        for (int i = 1, cnt = 0; i < n; ++i) {
+            cnt += boxes[i - 1] == '1';
+            ans[i] = ans[i - 1] + cnt;
+        }
+        for (int i = n - 2, cnt = 0, s = 0; ~i; --i) {
+            cnt += boxes[i + 1] == '1';
+            s += cnt;
+            ans[i] += s;
+        }
+        return ans;
+    }
+};
