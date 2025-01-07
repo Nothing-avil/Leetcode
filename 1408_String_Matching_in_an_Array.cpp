ce5d2ec69786353cd9ede5182bcad53fb3c75233
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-07 11:36:57 +0530

    Create 1408_String_Matching_in_an_Array.cpp

---
+++ 1408_String_Matching_in_an_Array.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for(int i=0; i<words.size(); i++){
+            for(int j=0; j<words.size(); j++){
+                if(j!=i){
+                    if (words[j].find(words[i]) != std::string::npos) {
+                        ans.push_back(words[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
