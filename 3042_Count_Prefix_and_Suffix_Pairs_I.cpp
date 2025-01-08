48271427e031ae5c07e8f0d90b3ca65c826f361b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-08 23:22:22 +0530

    Create 3042_Count_Prefix_and_Suffix_Pairs_I.cpp

---
+++ 3042_Count_Prefix_and_Suffix_Pairs_I.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool solve(string str1, string str2){
+        int x= str1.size();
+        int y= str2.size();
+        if(str1== str2.substr(0,x) && str1== str2.substr((y-x), y)){
+            return true;
+        }
+        return false;
+    }
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int c=0;
+        int n= words.size();
+        for(int i=0; i<n; i++){
+            int x= words[i].size();
+            for(int j=i+1; j<n; j++){
+                c+= solve(words[i], words[j]);
+            }
+        }
+        return c;
+    }
+};
