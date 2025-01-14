7e58d929778969fde170cafa8acd9eed4d733adc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-14 22:38:13 +0530

    Create 2657_Find_the_Prefix_Common_Array_of_Two_Arrays.cpp

---
+++ 2657_Find_the_Prefix_Common_Array_of_Two_Arrays.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        vector<int> cnt(A.size()+1), res(A.size());
+        int c=0;
+        for(int i=0; i<A.size(); i++){
+            cnt[A[i]]++;
+            if(cnt[A[i]] >= 2){
+                c++;
+            }
+            cnt[B[i]]++;
+            if(cnt[B[i]] >= 2){
+                c++;
+            }
+            res[i] = c;
+        }
+        return res;
+    }
+};
