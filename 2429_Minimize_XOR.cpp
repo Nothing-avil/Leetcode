7178caa47b8a0145a243fb5a14908ff34fea4ab5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-15 23:03:11 +0530

    Create 2429_Minimize_XOR.cpp

---
+++ 2429_Minimize_XOR.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        while(true){
+            int n = __builtin_popcount(num1);
+            int m = __builtin_popcount(num2);
+            if(n == m){
+                return num1;
+            }
+            else if(n > m){
+                num1 = num1&(num1 - 1);
+            }
+            else{
+                num1 = num1|(num1 + 1);
+            }
+        }
+        return -1;
+    }
+};
