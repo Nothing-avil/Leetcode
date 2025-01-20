bee51420362a31d6eff999acf1dc3d0b45edc626
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-20 23:14:03 +0530

    Create 2661_First_Completely_Painted_Row_or_Column.cpp

---
+++ 2661_First_Completely_Painted_Row_or_Column.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<pair<int, int>> mp(n*m);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                mp[mat[i][j] -1] = make_pair(i, j);
+            }
+        }
+        vector<int> row(n), col(m);
+        for(int i=0; i<arr.size(); i++){
+            auto [a, b] = mp[arr[i]-1];
+            row[a]++;
+            col[b]++;
+            if(col[b] == n || row[a] == m){
+                return i;
+            }
+        }
+        return arr.size();
+    }
+};
