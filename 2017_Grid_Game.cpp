55b3429f2f4e832571320ac7d29eccaf7cf889e8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-22 00:22:13 +0530

    Create 2017_Grid_Game.cpp

---
+++ 2017_Grid_Game.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long ans = LLONG_MAX;
+        long long s1=0, s2=0;
+        for(int i : grid[0]){
+            s1 += i;
+        }
+        for(int i=0; i<grid[0].size(); i++){
+            s1 -= grid[0][i];
+            ans = min(ans, max(s1,s2));
+            s2 += grid[1][i];
+        }
+        return ans;
+    }
+};
