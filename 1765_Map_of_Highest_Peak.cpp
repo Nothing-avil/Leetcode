7f83b9c191b6b7b6e4b9a8e0af91bda83353e30a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-22 22:45:50 +0530

    Create 1765_Map_of_Highest_Peak.cpp

---
+++ 1765_Map_of_Highest_Peak.cpp
@@ -0,0 +1,47 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        std::vector<std::vector<int>> distance(rows, std::vector<int>(cols, -1));
+        std::queue<std::pair<int, int>> q;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 1) {
+                    q.push({i, j});
+                    distance[i][j] = 0;
+                }
+            }
+        }
+        std::vector<int> dx = {-1, 1, 0, 0};
+        std::vector<int> dy = {0, 0, -1, 1};
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && distance[nx][ny] == -1) {
+                    distance[nx][ny] = distance[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        return distance;
+    }
+};
