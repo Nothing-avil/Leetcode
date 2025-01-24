965c33fec76ed5afc9eac313b3f32a5670e52fdc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-25 00:06:01 +0530

    Create 0802_Find_Eventual_Safe_States.cpp

---
+++ 0802_Find_Eventual_Safe_States.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> color;
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        color.assign(n, 0);
+        vector<int> ans;
+        for (int i = 0; i < n; ++i)
+            if (dfs(i, graph)) ans.push_back(i);
+        return ans;
+    }
+
+    bool dfs(int i, vector<vector<int>>& g) {
+        if (color[i]) return color[i] == 2;
+        color[i] = 1;
+        for (int j : g[i])
+            if (!dfs(j, g)) return false;
+        color[i] = 2;
+        return true;
+    }
+};
