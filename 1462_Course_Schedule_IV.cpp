5deef4c1dd662495646ef7651307b9bfeda57fde
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-27 19:25:10 +0530

    Create 1462_Course_Schedule_IV.cpp

---
+++ 1462_Course_Schedule_IV.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        bool dp[numCourses][numCourses];
+        memset(dp, false, sizeof(dp));
+        for(auto p : prerequisites){
+            dp[p[0]][p[1]] = true;
+        }
+        for(int k=0; k<numCourses ; k++){
+            for(int i=0; i<numCourses; i++){
+                for(int j= 0; j<numCourses; j++){
+                    dp[i][j] |= (dp[i][k] && dp[k][j]);
+                }
+            }
+        }
+        vector<bool> res;
+        for(auto q : queries){
+            res.push_back(dp[q[0]][q[1]]);
+        }
+        return res;
+    }
+};
