217374b427885b387d6b275f5ef067fffa69b755
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-02 21:57:22 +0530

    Create 1752_Check_if_Array_Is_Sorted_and_Rotated.cpp

---
+++ 1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        if(is_sorted(nums.begin(), nums.end())){
+            return true;
+        }
+        int n = nums.size();
+        int i=0;
+        if(nums[0] < nums[n-1]){
+            return false;
+        }
+        while(nums[i] <= nums[i+1]){
+            i++;
+        }
+        i++;
+        while(i < n-1){
+            if(nums[i] > nums[i+1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+};
