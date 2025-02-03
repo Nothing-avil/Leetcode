813406b580992397aa63b607956fcc85a7ae62db
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-03 22:57:14 +0530

    Create 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray.cpp

---
+++ 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int longs=1, in=1, de=1;
+        for(int i=1;i<nums.size(); i++){
+            if(nums[i]>nums[i-1]){
+                in++;
+                de=1;
+            }
+            else if(nums[i]<nums[i-1]){
+                de++;
+                in=1;
+            }
+            else{
+                in=1;
+                de=1;
+            }
+            longs= max({longs, in, de});
+        }
+        return longs;
+    }
+};
