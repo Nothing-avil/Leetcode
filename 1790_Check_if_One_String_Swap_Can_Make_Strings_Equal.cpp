a1e618ce2c9e77f5f9fc52b8bce1ff4887858df2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-05 18:31:32 +0530

    Create 1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.cpp

---
+++ 1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.cpp
@@ -0,0 +1,41 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int c[26] = {0};
+        for(char s : s1){
+            c[s-'a']++;
+        }
+        for(char s : s2){
+            c[s-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(c[i]){
+                return false;
+            }
+        }
+        int ss = 0;
+        for(int i=0; i<s1.size(); i++){
+            if(s1[i] != s2[i]){
+                ss++;
+            }
+            if(ss > 2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
