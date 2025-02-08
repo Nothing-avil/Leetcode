3fc8f669a3fedc744cb8f1dd7ffee40e147c928d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-08 22:39:19 +0530

    Create 2349_Design_a_Number_Container_System.cpp

---
+++ 2349_Design_a_Number_Container_System.cpp
@@ -0,0 +1,45 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class NumberContainers {
+public:
+    unordered_map<int,int> mp;
+    unordered_map<int, set<int>> pos;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        if(mp.contains(index)){
+            int val = mp[index];
+            pos[val].erase(index);
+            if(pos[val].empty()){
+                pos.erase(val);
+            }
+        }
+        mp[index] = number;
+        pos[number].insert(index);
+    }
+    
+    int find(int number) {
+        return pos.contains(number) ? *pos[number].begin() : -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
