7da23c0a7a8fc1078ad012f997463244992dc299
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-10 19:04:02 +0530

    Create 3174_Clear_Digits.cpp

---
+++ 3174_Clear_Digits.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(isdigit(ch)){
+                if(!st.empty()){
+                    st.pop();
+                }
+                continue;
+            }
+            st.push(ch);
+        }
+        if(st.size() == 0){
+            return "";
+        }
+        string ans = "";
+        while(st.size()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
