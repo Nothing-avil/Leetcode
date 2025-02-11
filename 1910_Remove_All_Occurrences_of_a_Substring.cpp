0094aced36d8c10466affbed73f2d00c3c271286
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-11 22:57:04 +0530

    Create 1910_Remove_All_Occurrences_of_a_Substring.cpp

---
+++ 1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string str = "";
+        int n = part.size();
+        for(char ch : s){
+            str+= ch;
+            if(str.size() >= n && part == str.substr(str.size() - n)){
+                str.erase(str.size() - n);
+            }
+        }
+        return str;
+    }
+};
