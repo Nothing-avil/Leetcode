e906897ad9297f26fd7ec9160b1f7c4a50640407
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-12 22:42:07 +0530

    Create 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.cpp

---
+++ 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+
+    int findsum(int n){
+        int s = 0;
+        while(n>0){
+            s += n%10;
+            n /= 10;
+        }
+        return s;
+    }
+
+    int maximumSum(vector<int>& nums) {
+        int res = -1;
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            int c = findsum(nums[i]);
+            if(mp.count(c)){
+                res = max(res, mp[c] + nums[i]);
+            }
+            mp[c] = max(mp[c], nums[i]);
+        }
+        return res;
+    }
+};
