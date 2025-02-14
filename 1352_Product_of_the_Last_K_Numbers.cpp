a564a19eb6bd9ec9a05cdc37b99070371df87944
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-14 22:43:02 +0530

    Create 1352_Product_of_the_Last_K_Numbers.cpp

---
+++ 1352_Product_of_the_Last_K_Numbers.cpp
@@ -0,0 +1,45 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class ProductOfNumbers {
+public:
+    vector<int> res;
+    ProductOfNumbers() {
+        res.push_back(1);
+    }
+    
+    void add(int num) {
+        if(num == 0){
+            res.clear();
+            res.push_back(1);
+            return;
+        }
+        res.push_back(res.back()*num);
+    }
+    
+    int getProduct(int k) {
+        int n = res.size();
+        if(n <= k){
+            return 0;
+        }
+        return res.back()/res[n-k-1];
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
