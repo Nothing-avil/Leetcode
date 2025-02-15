f7f6543220862aa2d131392b5f3d630c0e0bc9a4
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-15 19:28:41 +0530

    Create 2698_Find_the_Punishment_Number_of_an_Integer.cpp

---
+++ 2698_Find_the_Punishment_Number_of_an_Integer.cpp
@@ -0,0 +1,46 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+
+    bool test(string s, int i, int val){
+        if(i >= s.size()){
+            return val == 0;
+        }
+        int sum = 0;
+        for(int j = i; j<s.size(); j++){
+            sum = sum*10 + s[j] - '0';
+            if(sum > val){
+                break;
+            }
+            if(test(s, j+1, val - sum)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int punishmentNumber(int n) {
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            int powe = i*i;
+            string str = to_string(powe);
+            if(test(str, 0, i)){
+                ans += powe;
+            }
+        }
+        return ans;
+    }
+};
