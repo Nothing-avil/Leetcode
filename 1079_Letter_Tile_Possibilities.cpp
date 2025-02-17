6b1430507e5977c2cde922c0ff72869a433236c9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-17 12:22:17 +0530

    Create 1079_Letter_Tile_Possibilities.cpp

---
+++ 1079_Letter_Tile_Possibilities.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        vector<int> ch(26);
+        for(char c : tiles){
+            ch[c-'A']++;
+        }
+        int ans=0;
+        function<void()> solve = [&](){
+            ans++;
+            for(int i=0; i<26; i++){
+                if(ch[i] == 0){
+                    continue;
+                }
+                ch[i]--;
+                solve();
+                ch[i]++;
+            }
+        };
+        solve();
+        return ans-1;
+    }
+};
