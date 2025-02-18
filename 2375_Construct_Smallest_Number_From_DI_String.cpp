55a352a09c9169e75a0777eb39dfeca2a42bb680
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-18 23:12:56 +0530

    Create 2375_Construct_Smallest_Number_From_DI_String.cpp

---
+++ 2375_Construct_Smallest_Number_From_DI_String.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        string temp = "123456789";
+        int n = pattern.size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(pattern[j] == 'D' && temp[j] < temp[j+1]){
+                    swap(temp[j], temp[j+1]);
+                }
+            }
+        }
+        return temp.substr(0, n+1);
+    }
+};
