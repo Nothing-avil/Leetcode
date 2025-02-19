99fc6ade6c62a8145a158b7c5424aa07af17ab40
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-19 23:44:38 +0530

    Create 1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp

---
+++ 1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        vector<string> res;
+        string s = "";
+        function<void()> solve = [&](){
+            if(s.size() == n){
+                res.push_back(s);
+                return;
+            }
+            for(char c = 'a'; c <= 'c'; c++){
+                if(!s.empty() && s.back() == c){
+                    continue;
+                }
+                s.push_back(c);
+                solve();
+                s.pop_back();
+            }
+        };
+        solve();
+        if(res.size() < k){
+            return "";
+        }
+        return res[k-1];
+    }
+};
