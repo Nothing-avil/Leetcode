3fa0d30a6748b72a5ce508867fbbcc3c38ef30aa
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-21 23:06:29 +0530

    Create 1261_Find_Elements_in_a_Contaminated_Binary_Tree.cpp

---
+++ 1261_Find_Elements_in_a_Contaminated_Binary_Tree.cpp
@@ -0,0 +1,57 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    FindElements(TreeNode* root) {
+        root->val = 0;
+        dfs(root);
+    }
+
+    bool find(int target) {
+        return s.contains(target);
+    }
+
+private:
+    unordered_set<int> s;
+
+    void dfs(TreeNode* root) {
+        s.insert(root->val);
+        if (root->left) {
+            root->left->val = root->val * 2 + 1;
+            dfs(root->left);
+        }
+        if (root->right) {
+            root->right->val = root->val * 2 + 2;
+            dfs(root->right);
+        }
+    };
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
