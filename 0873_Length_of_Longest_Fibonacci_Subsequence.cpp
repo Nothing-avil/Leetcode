0c6558d0aac72d8783c648ba2f69c8c858b5dd81
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-27 23:38:10 +0530

    Create 0873_Length_of_Longest_Fibonacci_Subsequence.cpp

---
+++ 0873_Length_of_Longest_Fibonacci_Subsequence.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]] = i;
+        }
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(n, 2));
+        for(int i = 0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int di = arr[j] - arr[i];
+                if(di < arr[i] && mp.count(di)){
+                    int k = mp[di];
+                    dp[i][j] = dp[k][i] + 1;
+                    ans = max(ans , dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
