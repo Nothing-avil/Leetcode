6dcd4f529225a0ec834517c52bf4fa283f4bfb54
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-06 18:55:31 +0530

    Create 2965_Find_Missing_and_Repeated_Values.cpp

---
+++ 2965_Find_Missing_and_Repeated_Values.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> temp(n*n + 1);
+        int dup = -1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                temp[grid[i][j]]++;
+                if(temp[grid[i][j]] == 2){
+                    dup = grid[i][j];
+                }
+            }
+        }
+        for(int i = 1; i <= n*n; i++){
+            if(temp[i] == 0){
+                return {dup, i};
+            }
+        }
+        return {-1, -1};
+    }
+};
