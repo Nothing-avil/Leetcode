8f26f4c1ee482e9f49db5d315ed31916b62f745e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-09 00:08:07 +0530

    Create 2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp

---
+++ 2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int w =0;
+        for(int i=0; i<k; i++){
+            if(blocks[i] == 'W'){
+                w++;
+            }
+        }
+        int ans = w;
+        for(int i=k; i<blocks.size(); i++){
+            if(blocks[i] == 'W'){
+                w++;
+            }
+            if(blocks[i - k] == 'W'){
+                w--;
+            }
+            ans = min(ans, w);
+        }
+        return ans;
+    }
+};
