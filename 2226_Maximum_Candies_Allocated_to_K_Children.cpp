5308b85b089233d314b530b21eccc91c61938d63
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-14 22:39:08 +0530

    Create 2226_Maximum_Candies_Allocated_to_K_Children.cpp

---
+++ 2226_Maximum_Candies_Allocated_to_K_Children.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int l=0;
+        int h = *max_element(candies.begin(), candies.end());
+        while(l<h){
+            long long req = 0;
+            int mid = (l + h + 1) >> 1;
+            for(int c : candies){
+                req += (c/mid);
+            }
+            if(req >= k){
+                l = mid;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        return l;
+    }
+};
