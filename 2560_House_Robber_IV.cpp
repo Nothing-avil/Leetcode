e2deef4e2a298a8f44cb0f06c3f2a16bac9885a9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-15 23:24:42 +0530

    Create 2560_House_Robber_IV.cpp

---
+++ 2560_House_Robber_IV.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        auto f = [&](int x) {
+            int cnt = 0, j = -2;
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] > x || i == j + 1) {
+                    continue;
+                }
+                ++cnt;
+                j = i;
+            }
+            return cnt >= k;
+        };
+        int left = 0, right = *max_element(nums.begin(), nums.end());
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (f(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
