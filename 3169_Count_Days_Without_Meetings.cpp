2741acc4265511036f357f9a30b421be49189fe9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-24 21:32:17 +0530

    Create 3169_Count_Days_Without_Meetings.cpp

---
+++ 3169_Count_Days_Without_Meetings.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int result=0;
+        vector<vector<int>> qu;
+        for(auto v: meetings){
+            if(qu.size()==0 || qu.back()[1] < v[0]){
+                qu.push_back(v);
+            }
+            else{
+                qu.back()[1]= max(qu.back()[1], v[1]);
+            }
+        }
+        for(int i=0; i<qu.size(); i++){
+            result += qu[i][1] - qu[i][0] +1;
+        }
+        return days- result;
+    }
+};
