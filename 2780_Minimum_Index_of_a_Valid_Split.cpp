f75b8521410a5d6f7878643b3b492f202168dcd1
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-27 22:46:11 +0530

    Create 2780_Minimum_Index_of_a_Valid_Split.cpp

---
+++ 2780_Minimum_Index_of_a_Valid_Split.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int x = 0, cnt = 0;
+        unordered_map<int, int> freq;
+        for (int v : nums) {
+            ++freq[v];
+            if (freq[v] > cnt) {
+                cnt = freq[v];
+                x = v;
+            }
+        }
+        int cur = 0;
+        for (int i = 1; i <= nums.size(); ++i) {
+            if (nums[i - 1] == x) {
+                ++cur;
+                if (cur * 2 > i && (cnt - cur) * 2 > nums.size() - i) {
+                    return i - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
