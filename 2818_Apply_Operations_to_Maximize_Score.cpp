54d3a00c0f3ef59b1ec81cbc416c06e6ec00e178
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-30 00:10:44 +0530

    Create 2818_Apply_Operations_to_Maximize_Score.cpp

---
+++ 2818_Apply_Operations_to_Maximize_Score.cpp
@@ -0,0 +1,90 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int primeFactors(int n) {
+        int i = 2;
+        unordered_set<int> ans;
+        while (i <= n / i) {
+            while (n % i == 0) {
+                ans.insert(i);
+                n /= i;
+            }
+            ++i;
+        }
+        if (n > 1) {
+            ans.insert(n);
+        }
+        return ans.size();
+    }
+
+    int maximumScore(vector<int>& nums, int k) {
+        const int mod = 1e9 + 7;
+        int n = nums.size();
+        vector<tuple<int, int, int>> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = {i, primeFactors(nums[i]), nums[i]};
+        }
+        vector<int> left(n, -1);
+        vector<int> right(n, n);
+        stack<int> stk;
+        for (auto [i, f, _] : arr) {
+            while (!stk.empty() && get<1>(arr[stk.top()]) < f) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                left[i] = stk.top();
+            }
+            stk.push(i);
+        }
+        stk = stack<int>();
+        for (int i = n - 1; ~i; --i) {
+            int f = get<1>(arr[i]);
+            while (!stk.empty() && get<1>(arr[stk.top()]) <= f) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                right[i] = stk.top();
+            }
+            stk.push(i);
+        }
+        sort(arr.begin(), arr.end(), [](const auto& lhs, const auto& rhs) {
+            return get<2>(rhs) < get<2>(lhs);
+        });
+        long long ans = 1;
+        auto qpow = [&](long long a, int n) {
+            long long ans = 1;
+            for (; n; n >>= 1) {
+                if (n & 1) {
+                    ans = ans * a % mod;
+                }
+                a = a * a % mod;
+            }
+            return ans;
+        };
+        for (auto [i, _, x] : arr) {
+            int l = left[i], r = right[i];
+            long long cnt = 1LL * (i - l) * (r - i);
+            if (cnt <= k) {
+                ans = ans * qpow(x, cnt) % mod;
+                k -= cnt;
+            } else {
+                ans = ans * qpow(x, k) % mod;
+                break;
+            }
+        }
+        return ans;
+    }
+};
