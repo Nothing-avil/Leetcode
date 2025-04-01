7701165f1fd6ca8ab94e04e40773e00f4f57ac3f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-01 23:21:55 +0530

    Create 2140_Solving_Questions_With_Brainpower.cpp

---
+++ 2140_Solving_Questions_With_Brainpower.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        long long n = questions.size();
+        long long store[100001];
+        memset(store, -1, sizeof(store));
+        function<long long(int)> solver = [&](long long idx) -> long long{
+            if(idx >= n){
+                return 0;
+            }
+            if(store[idx] != -1){
+                return store[idx];
+            }
+            long long take = questions[idx][0] + solver(idx + questions[idx][1] + 1);
+            long long nottake = solver(idx+1);
+            return store[idx] = max(take, nottake);
+        };
+        return solver(0);
+    }
+};
