8dc60f11e427b30030a0558577fab5851cd4522b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-02 23:32:33 +0530

    Create 2873_Maximum_Value_of_an_Ordered_Triplet_I.cpp

---
+++ 2873_Maximum_Value_of_an_Ordered_Triplet_I.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long answer = 0;
+        for (int i=0; i<nums.size(); i++){
+            for(int j = i+1; j<nums.size(); j++){
+                for(int k = j+1; k<nums.size(); k++){
+                    answer = max(answer, (long long)(nums[i] - nums[j])*nums[k]);
+                }
+            }
+        }
+        return answer;
+    }
+};
