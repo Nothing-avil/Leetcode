400f940ab91d533b8cd445b4c17fc6adbab39ecf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-09 16:33:54 +0530

    Create 3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.cpp

---
+++ 3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int val = INT_MAX;
+        for(int i : nums){
+            freq[i]++;
+            val = min(val, i);
+        }
+        if(val < k){
+            return -1;
+        }
+        int answer = freq.size();
+        if(freq[k]){
+            answer--;
+        }
+        return answer;
+    }
+};
