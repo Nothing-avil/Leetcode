de4903452bc07c41d90f076e430cc552d6753fd9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-11 23:08:06 +0530

    Create 2843_Count_Symmetric_Integers.cpp

---
+++ 2843_Count_Symmetric_Integers.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int ans = 0;
+        function<bool(int)> f = [](int x) {
+            string s = to_string(x);
+            int n = s.size();
+            if (n & 1) {
+                return 0;
+            }
+            int a = 0, b = 0;
+            for (int i = 0; i < n / 2; ++i) {
+                a += s[i] - '0';
+                b += s[n / 2 + i] - '0';
+            }
+            return a == b ? 1 : 0;
+        };
+        for (int x = low; x <= high; ++x) {
+            ans += f(x);
+        }
+        return ans;
+    }
+};
