ab17f400083743690ec83c31d9c74c196b13afa3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-16 23:33:38 +0530

    Create 2537_Count_the_Number_of_Good_Subarrays.cpp

---
+++ 2537_Count_the_Number_of_Good_Subarrays.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n= nums.size();
+        int i=0, j =0, copy = 0;
+        long long answer = 0;
+        unordered_map<int, int> mp;
+        while(j < n){
+            copy += mp[nums[j]];
+            mp[nums[j]]++;
+            while(copy >= k){
+                answer += (n - j);
+                mp[nums[i]]--;
+                copy -= mp[nums[i++]];
+            }
+            j++;
+        }
+        return answer;
+    }
+};
