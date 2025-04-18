3fd60ccfd774abe23c60d45649d4ae10ed8631fc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-19 00:01:58 +0530

    Create 0038_Count_and_Say.cpp

---
+++ 0038_Count_and_Say.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string countAndSay(int n) {
+        string answer = "1";
+        while(--n){
+            string temp = "";
+            for(int i = 0; i<answer.size();){
+                int j = i;
+                while(j < answer.size() && answer[i] == answer[j]){
+                    j++;
+                }
+                temp += to_string(j-i);
+                temp += answer[i];
+                i = j;
+            }
+            answer = temp;
+        }
+        return answer;
+    }
+};
