22afbb35c022c5fe9d29a985588df4bab130fa65
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-23 20:36:09 +0530

    Create 1399_Count_Largest_Group.cpp

---
+++ 1399_Count_Largest_Group.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        int add[37] = {0};
+        int ans = 0, mx = 0;
+        for(int i = 1; i <= n; i++){
+            int sum =0;
+            for(int j = i; j; j /= 10){
+                sum += (j % 10);
+            }
+            add[sum]++;
+            if(mx < add[sum]){
+                mx = add[sum];
+                ans = 1;
+            }
+            else if(mx == add[sum]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
