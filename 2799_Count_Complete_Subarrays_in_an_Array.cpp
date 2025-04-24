c6641bf19d10c76defa105b508e514a6b9ba63a2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-24 23:56:12 +0530

    Create 2799_Count_Complete_Subarrays_in_an_Array.cpp

---
+++ 2799_Count_Complete_Subarrays_in_an_Array.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i : nums){
+            mp[i] = 1;
+        }
+        int n = mp.size();
+        mp.clear();
+        int answer =0;
+        for(int i=0, j = 0; j < nums.size(); j++){
+            mp[nums[j]]++;
+            while(mp.size() == n){
+                answer += (nums.size() - j);
+                if(--mp[nums[i]] == 0){
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+        }
+        return answer;
+    }
+};
