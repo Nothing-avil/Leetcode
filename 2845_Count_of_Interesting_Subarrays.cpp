dca28463f239e5cec81593bf12473a0cfc00c4f8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-26 00:16:58 +0530

    Create 2845_Count_of_Interesting_Subarrays.cpp

---
+++ 2845_Count_of_Interesting_Subarrays.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        int n = nums.size();
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = int(nums[i] % modulo == k);
+        }
+        unordered_map<int, int> cnt;
+        cnt[0] = 1;
+        long long ans = 0;
+        int s = 0;
+        for (int x : arr) {
+            s += x;
+            ans += cnt[(s - k + modulo) % modulo];
+            cnt[s % modulo]++;
+        }
+        return ans;
+
+    }
+};
