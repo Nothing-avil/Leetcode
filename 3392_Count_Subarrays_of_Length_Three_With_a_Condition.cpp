f7f340dadaa455322c6e2d00fc1d2deecc71dfce
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-27 14:12:49 +0530

    Create 3392_Count_Subarrays_of_Length_Three_With_a_Condition.cpp

---
+++ 3392_Count_Subarrays_of_Length_Three_With_a_Condition.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int i = 2;
+        int count = 0;
+        int n = nums.size();
+        while( i < n){
+            if( (nums[i] + nums[i-2]) * 2 == (nums[i-1])){
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+};
