42d4f4d8fd3952bd75ec76f1ad27aed655a12ce0
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-29 00:19:17 +0530

    Create 2302_Count_Subarrays_With_Score_Less_Than_K.cpp

---
+++ 2302_Count_Subarrays_With_Score_Less_Than_K.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        long long answer = 0, sum = 0;
+        for(int i=0, j = 0; i< nums.size(); i++){
+            sum += nums[i];
+            while(sum * (i-j+1) >= k){
+                sum -= nums[j++];
+            }
+            answer += (i - j + 1);
+        }
+        return answer;
+    }
+};
