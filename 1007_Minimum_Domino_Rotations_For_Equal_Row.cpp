87c4bb5bb0eaeb28b8f4c8fca577844b22874e8b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-03 23:30:09 +0530

    Create 1007_Minimum_Domino_Rotations_For_Equal_Row.cpp

---
+++ 1007_Minimum_Domino_Rotations_For_Equal_Row.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int answer = INT_MAX;
+        for(int i=1; i<=6; i++){
+            int val = 0;
+            int top = 0, bottom = 0;
+            for(int j=0; j<tops.size(); j++){
+                if(tops[j] != i && bottoms[j] != i){
+                    val = -1;
+                    break;
+                }else if(tops[j] != i){
+                    top++;
+                }else if(bottoms[j] != i){
+                    bottom++;
+                }
+                val = min(top, bottom);
+            }
+            if(val != -1){
+                answer = min(answer, val);
+            }
+        }
+        return answer == INT_MAX ? -1 : answer;
+    }
+};
