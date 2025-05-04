e3bd4077f7c844d510a0d0634fac627a1f9f7210
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-04 19:56:29 +0530

    Create 1128_Number_of_Equivalent_Domino_Pairs.cpp

---
+++ 1128_Number_of_Equivalent_Domino_Pairs.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int temp[100] = {0};
+        int answer = 0;
+        for(auto i : dominoes){
+            int val = 0;
+            if(i[0] < i[1]){
+                val = i[0] * 10 + i[1];
+            }
+            else{
+                val = i[1] * 10 + i[0];
+            }
+            answer += temp[val];
+            temp[val]++;
+        }
+        return answer;
+    }
+};
