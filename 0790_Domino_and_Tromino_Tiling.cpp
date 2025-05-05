d44cf89a9baed71c11d25913a3cad1bfb3356d8d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-06 00:02:14 +0530

    Create 0790_Domino_and_Tromino_Tiling.cpp

---
+++ 0790_Domino_and_Tromino_Tiling.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    int numTilings(int n) {
+        long f[4] = {1, 0, 0, 0};
+        for (int i = 1; i <= n; ++i) {
+            long g[4] = {0, 0, 0, 0};
+            g[0] = (f[0] + f[1] + f[2] + f[3]) % mod;
+            g[1] = (f[2] + f[3]) % mod;
+            g[2] = (f[1] + f[3]) % mod;
+            g[3] = f[0];
+            memcpy(f, g, sizeof(g));
+        }
+        return f[0];
+    }
+};
