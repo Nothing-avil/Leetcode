deb387b467962ff3d21751fcfc95d99a1969f077
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-10 23:28:37 +0530

    Create 2918_Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp

---
+++ 2918_Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long answer1 = 0, answer2 = 0;
+        bool check = false;
+        for(int i : nums1){
+            check |= i == 0;
+            answer1 += max(i, 1);
+        }
+        for(int i : nums2){
+            answer2 += max(i, 1);
+        }
+        if(answer1 > answer2){
+            return minSum(nums2, nums1);
+        }
+        if(answer1 == answer2){
+            return answer1;
+        }
+        return check ? answer2 : -1;
+    }
+};
