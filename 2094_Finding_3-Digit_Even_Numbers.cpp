5217720b6d19cc9de1f77822d9035552e0eeb21b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-12 23:12:36 +0530

    Create 2094_Finding_3-Digit_Even_Numbers.cpp

---
+++ 2094_Finding_3-Digit_Even_Numbers.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        int cnt[10]{};
+        for (int x : digits) {
+            ++cnt[x];
+        }
+        vector<int> ans;
+        for (int x = 100; x < 1000; x += 2) {
+            int cnt1[10]{};
+            for (int y = x; y; y /= 10) {
+                ++cnt1[y % 10];
+            }
+            bool ok = true;
+            for (int i = 0; i < 10 && ok; ++i) {
+                ok = cnt[i] >= cnt1[i];
+            }
+            if (ok) {
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
