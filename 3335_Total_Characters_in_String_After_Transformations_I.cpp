7823ebb964e1c1f18186b3375933ecf4d8dd9822
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-13 23:19:03 +0530

    Create 3335_Total_Characters_in_String_After_Transformations_I.cpp

---
+++ 3335_Total_Characters_in_String_After_Transformations_I.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        const int mod = 1e9 + 7;
+        int freq[26] = {0};
+        for(char ch : s){
+            freq[ch - 'a']++;
+        }
+        for(int i = 0; i<t; i++){
+            int test = freq[25];
+            for(int j=25; j>0; j--){
+                freq[j] = freq[j-1];
+            }
+            freq[0] = 0;
+            freq[1] = (freq[1] + test) % mod;
+            freq[0] = (freq[0] + test) % mod;
+        }
+        int answer = 0;
+        for(int i=0; i<26; i++){
+            answer = (answer + freq[i]) % mod;
+        }
+        return answer;
+    }
+};
