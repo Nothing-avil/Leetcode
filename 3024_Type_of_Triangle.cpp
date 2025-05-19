5ca273495fea5f88d7c736c1e2fc73f54e3d3c9d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-19 23:58:34 +0530

    Create 3024_Type_of_Triangle.cpp

---
+++ 3024_Type_of_Triangle.cpp
@@ -0,0 +1,24 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        assert(nums.size() == 3 && nums[0] >= 1 && nums[2] <= 100);
+        if (nums[0] + nums[1] <= nums[2]) return "none";
+        if (nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
+        return nums[0] == nums[1] || nums[1] == nums[2] ? "isosceles" : "scalene";
+    }
+};
