52557c665a903f8d9c481b93c40ed97b6007fbf2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-25 22:31:33 +0530

    Create 2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words.cpp

---
+++ 2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> mp;
+        for(string ch : words){
+            mp[ch]++;
+        }
+        int answer = 0;
+        int x =0;
+        for(auto[a, v] : mp){
+            string ra = a;
+            reverse(ra.begin(), ra.end());
+            if(a[1] == a[0]){
+                x += v & 1;
+                answer += (v/2)*2*2;
+            }
+            else if(mp.count(ra)){
+                answer += min(v, mp[ra]) * 2;
+            }
+        }
+        answer += x ? 2 : 0;
+        return answer;
+    }
+};
