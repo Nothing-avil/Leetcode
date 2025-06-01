075e5fb78f6e48933381fa09235d58be71efaf14
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-01 23:13:49 +0530

    Create 2929_Distribute_Candies_Among_Children_II.cpp

---
+++ 2929_Distribute_Candies_Among_Children_II.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        auto comb2 = [](int n) {
+            return 1LL * n * (n - 1) / 2;
+        };
+        if (n > 3 * limit) {
+            return 0;
+        }
+        long long ans = comb2(n + 2);
+        if (n > limit) {
+            ans -= 3 * comb2(n - limit + 1);
+        }
+        if (n - 2 >= 2 * limit) {
+            ans += 3 * comb2(n - 2 * limit);
+        }
+        return ans;
+    }
+};
