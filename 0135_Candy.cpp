32edecd47d69930e3cfac0e133c0734b6ea83e82
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-02 23:14:08 +0530

    Create 0135_Candy.cpp\

---
+++ 0135_Candy.cpp
@@ -0,0 +1,36 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        const int n = ratings.size();
+    int ans = 0;
+    vector<int> l(n, 1);
+    vector<int> r(n, 1);
+
+    for (int i = 1; i < n; ++i)
+      if (ratings[i] > ratings[i - 1])
+        l[i] = l[i - 1] + 1;
+
+    for (int i = n - 2; i >= 0; --i)
+      if (ratings[i] > ratings[i + 1])
+        r[i] = r[i + 1] + 1;
+
+    for (int i = 0; i < n; ++i)
+      ans += max(l[i], r[i]);
+
+    return ans;
+    }
+};
