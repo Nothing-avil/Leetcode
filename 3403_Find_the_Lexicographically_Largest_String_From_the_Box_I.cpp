3b3e759263d4f63871e189ac84ce179f5f710af9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-04 23:41:04 +0530

    Create 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.cpp

---
+++ 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int n = word.size();
+        string ans;
+        for (int i = 0; i < n; ++i) {
+            int k = min(n - i, n - numFriends + 1);
+            string t = word.substr(i, k);
+            ans = max(ans, t);
+        }
+        return ans;
+    }
+};
