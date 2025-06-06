5c43b66850bf3a443e7bb7f5047a90a1a70bbba2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-07 00:20:16 +0530

    Create 2434_Using_a_Robot_to_Print_the_Lexicographically_Smallest_String.cpp

---
+++ 2434_Using_a_Robot_to_Print_the_Lexicographically_Smallest_String.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string robotWithString(string s) {
+        int cnt[26] = {0};
+        for (char& c : s) ++cnt[c - 'a'];
+        char mi = 'a';
+        string stk;
+        string ans;
+        for (char& c : s) {
+            --cnt[c - 'a'];
+            while (mi < 'z' && cnt[mi - 'a'] == 0) ++mi;
+            stk += c;
+            while (!stk.empty() && stk.back() <= mi) {
+                ans += stk.back();
+                stk.pop_back();
+            }
+        }
+        return ans;
+    }
+};
