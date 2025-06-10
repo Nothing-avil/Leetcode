3c4bd72ef05da306f4adbcd53427f61afb65f75f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-10 23:50:21 +0530

    Create 3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp

---
+++ 3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxDifference(string s) {
+        int cnt[26]{};
+        for (char c : s) {
+            ++cnt[c - 'a'];
+        }
+        int a = 0, b = 1 << 30;
+        for (int v : cnt) {
+            if (v % 2 == 1) {
+                a = max(a, v);
+            } else if (v > 0) {
+                b = min(b, v);
+            }
+        }
+        return a - b;
+    }
+};
