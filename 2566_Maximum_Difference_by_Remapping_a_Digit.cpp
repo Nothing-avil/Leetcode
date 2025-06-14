6a8ecd9ba361bbfa911c61c9f422455dbfbe9e3c
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-14 23:20:24 +0530

    Create 2566_Maximum_Difference_by_Remapping_a_Digit.cpp

---
+++ 2566_Maximum_Difference_by_Remapping_a_Digit.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s = to_string(num);
+        string t = s;
+        char first = s[0];
+        for (char& c : s) {
+            if (c == first) {
+                c = '0';
+            }
+        }
+        int mi = stoi(s);
+        for (int i = 0; i < t.size(); ++i) {
+            if (t[i] != '9') {
+                char second = t[i];
+                for (int j = i; j < t.size(); ++j) {
+                    if (t[j] == second) {
+                        t[j] = '9';
+                    }
+                }
+                return stoi(t) - mi;
+            }
+        }
+        return num - mi;
+    }
+};
