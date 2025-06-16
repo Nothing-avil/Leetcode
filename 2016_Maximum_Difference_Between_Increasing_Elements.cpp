40e099299cfcad6c110f74db91975590e8b7ae71
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-16 22:41:34 +0530

    Create 2016_Maximum_Difference_Between_Increasing_Elements.cpp

---
+++ 2016_Maximum_Difference_Between_Increasing_Elements.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int answer = INT_MIN;
+        for(int i=0; i<nums.size() - 1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i] < nums[j]){
+                    answer = max(answer, (nums[j] - nums[i]));
+                }
+            }
+        }
+        return answer == INT_MIN ? -1 : answer;
+    }
+};
