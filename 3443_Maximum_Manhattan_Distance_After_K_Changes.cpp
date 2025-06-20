9f8ae8e2cb1dae7e761b558096ae4e1686aabe05
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-20 23:07:02 +0530

    Create 3443_Maximum_Manhattan_Distance_After_K_Changes.cpp

---
+++ 3443_Maximum_Manhattan_Distance_After_K_Changes.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxDistance(string s, int k) {
+        auto calc = [&](char a, char b) {
+            int ans = 0, mx = 0, cnt = 0;
+            for (char c : s) {
+                if (c == a || c == b) {
+                    ++mx;
+                } else if (cnt < k) {
+                    ++mx;
+                    ++cnt;
+                } else {
+                    --mx;
+                }
+                ans = max(ans, mx);
+            }
+            return ans;
+        };
+        int a = calc('S', 'E');
+        int b = calc('S', 'W');
+        int c = calc('N', 'E');
+        int d = calc('N', 'W');
+        return max({a, b, c, d});
+    }
+};
