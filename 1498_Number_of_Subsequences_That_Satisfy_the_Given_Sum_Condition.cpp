83e571e2f1a3a37a08045e819dcbc31c3bcb5d27
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-29 22:56:34 +0530

    Create 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.cpp

---
+++ 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
+        const int mod = 1e9+7;
+        int answer = 0;
+        sort(nums.begin(), nums.end());
+        vector<int> check(n, 1);
+        for(int i=1; i<n; i++){
+            check[i] = (check[i-1] * 2)%mod;
+        }
+        int i=0, j = n-1;
+        while(i<=j){
+            if(nums[i] + nums[j] > target){
+                j--;
+            }
+            else{
+                answer = (answer + check[j-i])%mod;
+                i++;
+            }
+        }
+        return answer;
+    }
+};
