944e7f8d2d4c11cfce48a3345cde601ec4ffa408
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-01 11:15:12 +0530

    Create 3330_Find_the_Original_Typed_String_I.cpp

---
+++ 3330_Find_the_Original_Typed_String_I.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int cnt[26] = {0};
+        for(int i=1; i<word.size(); i++){
+            if(word[i] == word[i-1]){
+                cnt[word[i] - 'a']++;
+            }
+        }
+        int answer = 1;
+        for(int i=0; i<26; i++){
+            answer += cnt[i];
+        }
+        return answer;
+    }
+};
