8708f6343bb5c60cd5b5cdc8d5c5ba71d3756c87
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-04 23:37:53 +0530

    Create 3307_Find_the_K-th_Character_in_String_Game_II.cpp

---
+++ 3307_Find_the_K-th_Character_in_String_Game_II.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        long long n = 1;
+        int i = 0;
+        while (n < k) {
+            n *= 2;
+            ++i;
+        }
+        int d = 0;
+        while (n > 1) {
+            if (k > n / 2) {
+                k -= n / 2;
+                d += operations[i - 1];
+            }
+            n /= 2;
+            --i;
+        }
+        return 'a' + (d % 26);
+    }
+};
