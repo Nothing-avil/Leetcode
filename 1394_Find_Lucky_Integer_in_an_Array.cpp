ed1e5ee7e79ec63d3501572cd5f9dee4fb4fc92f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-05 22:52:48 +0530

    Create 1394_Find_Lucky_Integer_in_an_Array.cpp

---
+++ 1394_Find_Lucky_Integer_in_an_Array.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int cnt[501] = {0};
+        int answer = -1;
+        for(int i : arr){
+            cnt[i]++;
+        }
+        for(int i=1; i<501; i++){
+            if(cnt[i] == i){
+                answer = max(answer, i);
+            }
+        }
+        return answer;
+    }
+};
