17c01775a13c6bedd6c5dfea5ad6e800332679c3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-06 22:25:43 +0530

    Create 1865_Finding_Pairs_With_a_Certain_Sum.cpp

---
+++ 1865_Finding_Pairs_With_a_Certain_Sum.cpp
@@ -0,0 +1,50 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class FindSumPairs {
+public:
+    vector<int> nums1, nums2;
+    unordered_map<int, int> mp;
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        this->nums1= nums1;
+        this->nums2 = nums2;
+        for(int i : nums2){
+            mp[i]++;
+        }
+    }
+    
+    void add(int index, int val) {
+        int v = nums2[index];
+        mp[v]--;
+        mp[v + val]++;
+        nums2[index] += val;
+    }
+    
+    int count(int tot) {
+        int res = 0;
+        for(int i = 0; i < nums1.size(); i++){
+            if(mp.contains(tot - nums1[i])){
+                res += mp[(tot - nums1[i])];
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
