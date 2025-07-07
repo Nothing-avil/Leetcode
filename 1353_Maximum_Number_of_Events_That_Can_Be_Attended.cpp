3a56b399d37956ae022c50409f75b9da5d033615
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-07 23:34:30 +0530

    Create 1353_Maximum_Number_of_Events_That_Can_Be_Attended.cpp

---
+++ 1353_Maximum_Number_of_Events_That_Can_Be_Attended.cpp
@@ -0,0 +1,42 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        unordered_map<int, vector<int>> d;
+        int i = INT_MAX, j = 0;
+        for (auto& v : events) {
+            int s = v[0], e = v[1];
+            d[s].push_back(e);
+            i = min(i, s);
+            j = max(j, e);
+        }
+        priority_queue<int, vector<int>, greater<int>> q;
+        int ans = 0;
+        for (int s = i; s <= j; ++s) {
+            while (q.size() && q.top() < s) {
+                q.pop();
+            }
+            for (int e : d[s]) {
+                q.push(e);
+            }
+            if (q.size()) {
+                ++ans;
+                q.pop();
+            }
+        }
+        return ans;
+    }
+};
