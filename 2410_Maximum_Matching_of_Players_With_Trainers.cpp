407ff811e4cb50a1594fca143c0f7a522a9a7258
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-14 00:06:04 +0530

    Create 2410_Maximum_Matching_of_Players_With_Trainers.cpp

---
+++ 2410_Maximum_Matching_of_Players_With_Trainers.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        ranges::sort(players);
+        ranges::sort(trainers);
+        int m = players.size(), n = trainers.size();
+        for (int i = 0, j = 0; i < m; ++i, ++j) {
+            while (j < n && trainers[j] < players[i]) {
+                ++j;
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return m;
+    }
+};
