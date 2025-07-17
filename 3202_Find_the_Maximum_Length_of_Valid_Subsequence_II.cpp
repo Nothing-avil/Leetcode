bd8fcf23b5dbcac3223e82fa3f5c3c338fc54f0e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-17 12:20:13 +0530

    Create 3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.cpp

---
+++ 3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int mod = k;
+        int dp[mod][mod];
+        int answer = 0;
+        memset(dp, 0, sizeof(dp));
+        for(int i : nums){
+            i %= mod;
+            for(int j = 0; j<mod; j++){
+                int c = (j - i + mod)%mod;
+                dp[i][c] = dp[c][i] + 1;
+                answer = max(answer, dp[i][c]);
+            }
+        }
+        return answer;
+    }
+};
