e647fb9d98f66ca2d0fc6ff59a29fd2dd581c7d6
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-19 00:18:34 +0530

    Create 2163_Minimum_Difference_in_Sums_After_Removal_of_Elements.cpp

---
+++ 2163_Minimum_Difference_in_Sums_After_Removal_of_Elements.cpp
@@ -0,0 +1,54 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+        int m = nums.size();
+        int n = m / 3;
+
+        using ll = long long;
+        ll s = 0;
+        ll pre[m + 1];
+        priority_queue<int> q1;
+        for (int i = 1; i <= n * 2; ++i) {
+            int x = nums[i - 1];
+            s += x;
+            q1.push(x);
+            if (q1.size() > n) {
+                s -= q1.top();
+                q1.pop();
+            }
+            pre[i] = s;
+        }
+        s = 0;
+        ll suf[m + 1];
+        priority_queue<int, vector<int>, greater<int>> q2;
+        for (int i = m; i > n; --i) {
+            int x = nums[i - 1];
+            s += x;
+            q2.push(x);
+            if (q2.size() > n) {
+                s -= q2.top();
+                q2.pop();
+            }
+            suf[i] = s;
+        }
+        ll ans = 1e18;
+        for (int i = n; i <= n * 2; ++i) {
+            ans = min(ans, pre[i] - suf[i + 1]);
+        }
+        return ans;
+    }
+};
