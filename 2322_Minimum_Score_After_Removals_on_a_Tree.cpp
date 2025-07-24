a40ab8a262b62ca1f1bf02cf9b0efddb4e084d6c
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-24 23:09:09 +0530

    Create 2322_Minimum_Score_After_Removals_on_a_Tree.cpp

---
+++ 2322_Minimum_Score_After_Removals_on_a_Tree.cpp
@@ -0,0 +1,60 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
+        int n = nums.size();
+        vector<int> g[n];
+        for (const auto& e : edges) {
+            int a = e[0], b = e[1];
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        int s = 0, s1 = 0;
+        int ans = INT_MAX;
+        for (int x : nums) {
+            s ^= x;
+        }
+        auto dfs = [&](this auto&& dfs, int i, int fa) -> int {
+            int res = nums[i];
+            for (int j : g[i]) {
+                if (j != fa) {
+                    res ^= dfs(j, i);
+                }
+            }
+            return res;
+        };
+        auto dfs2 = [&](this auto&& dfs2, int i, int fa) -> int {
+            int res = nums[i];
+            for (int j : g[i]) {
+                if (j != fa) {
+                    int s2 = dfs2(j, i);
+                    res ^= s2;
+                    int mx = max({s ^ s1, s2, s1 ^ s2});
+                    int mn = min({s ^ s1, s2, s1 ^ s2});
+                    ans = min(ans, mx - mn);
+                }
+            }
+            return res;
+        };
+        for (int i = 0; i < n; ++i) {
+            for (int j : g[i]) {
+                s1 = dfs(i, j);
+                dfs2(i, j);
+            }
+        }
+        return ans;
+    }
+};
