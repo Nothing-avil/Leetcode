9b28c9ad4dd4e295984ba38bbf0df1221dba3787
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-25 22:37:16 +0530

    Create 3487_Maximum_Unique_Subarray_Sum_After_Deletion.cpp

---
+++ 3487_Maximum_Unique_Subarray_Sum_After_Deletion.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i : nums){
+            mp[i]++;
+        }
+        int answer = nums[0];
+        int maxsum = nums[0];
+        int c = nums[0] < 0 ? 1 : 0;
+        mp.erase(nums[0]);
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i] < 0){
+                c++;
+                continue;
+            }
+            if(nums[i] >= 0 && mp[nums[i]]){
+                maxsum = max(nums[i], maxsum + nums[i]);
+                answer = max(answer, maxsum);
+                mp.erase(nums[i]);
+            }
+        }
+        return (c == nums.size()) ? *max_element(nums.begin(), nums.end()) : answer;
+    }
+};
