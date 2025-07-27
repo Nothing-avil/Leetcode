59dd5be31f9c7e859ffc245ee61607a7693d6a7b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-27 23:27:20 +0530

    Create 2210_Count_Hills_and_Valleys_in_an_Array.cpp

---
+++ 2210_Count_Hills_and_Valleys_in_an_Array.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+         int ans = 0;
+        for (int i = 1, j = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i + 1]) {
+                continue;
+            }
+            if (nums[i] > nums[j] && nums[i] > nums[i + 1]) {
+                ++ans;
+            }
+            if (nums[i] < nums[j] && nums[i] < nums[i + 1]) {
+                ++ans;
+            }
+            j = i;
+        }
+        return ans;
+    }
+};
