38cf467e53d1d5b25fd095ccee8f3e1224dfdd70
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-29 22:47:27 +0530

    Create 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.cpp

---
+++ 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n);
+        vector<int>bits(32, -1);
+        for(int i= n-1; ~i; i--){
+            int cal = 1;
+            for(int j=0; j<32; j++){
+                if((nums[i] >> j) & 1){
+                    bits[j] = i;
+                }
+                else{
+                    cal = max(cal, bits[j] - i + 1);
+                }
+            }
+            answer[i] = cal;
+        }
+        return answer;
+    }
+};
