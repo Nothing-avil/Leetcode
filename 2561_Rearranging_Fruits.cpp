8fb09f3de29ae158e77b74af61f9121a2292c170
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-03 00:00:37 +0530

    Create 2561_Rearranging_Fruits.cpp

---
+++ 2561_Rearranging_Fruits.cpp
@@ -0,0 +1,43 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        int n = basket1.size();
+        unordered_map<int, int> cnt;
+        for (int i = 0; i < n; ++i) {
+            cnt[basket1[i]]++;
+            cnt[basket2[i]]--;
+        }
+        int mi = 1 << 30;
+        vector<int> nums;
+        for (auto& [x, v] : cnt) {
+            if (v % 2) {
+                return -1;
+            }
+            for (int i = abs(v) / 2; i; --i) {
+                nums.push_back(x);
+            }
+            mi = min(mi, x);
+        }
+        sort(nums.begin(), nums.end());
+        int m = nums.size();
+        long long ans = 0;
+        for (int i = 0; i < m / 2; ++i) {
+            ans += min(nums[i], mi * 2);
+        }
+        return ans;
+    }
+};
