644334ea0f67246683bb749c394ab105fc8779be
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-06 23:09:28 +0530

    Create 3479_Fruits_Into_Baskets_III.cpp

---
+++ 3479_Fruits_Into_Baskets_III.cpp
@@ -0,0 +1,86 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class SegmentTree {
+public:
+    vector<int> nums, tr;
+
+    SegmentTree(vector<int>& nums) {
+        this->nums = nums;
+        int n = nums.size();
+        tr.resize(n * 4);
+        build(1, 1, n);
+    }
+
+    void build(int u, int l, int r) {
+        if (l == r) {
+            tr[u] = nums[l - 1];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(u * 2, l, mid);
+        build(u * 2 + 1, mid + 1, r);
+        pushup(u);
+    }
+
+    void modify(int u, int l, int r, int i, int v) {
+        if (l == r) {
+            tr[u] = v;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if (i <= mid) {
+            modify(u * 2, l, mid, i, v);
+        } else {
+            modify(u * 2 + 1, mid + 1, r, i, v);
+        }
+        pushup(u);
+    }
+
+    int query(int u, int l, int r, int v) {
+        if (tr[u] < v) {
+            return -1;
+        }
+        if (l == r) {
+            return l;
+        }
+        int mid = (l + r) >> 1;
+        if (tr[u * 2] >= v) {
+            return query(u * 2, l, mid, v);
+        }
+        return query(u * 2 + 1, mid + 1, r, v);
+    }
+
+    void pushup(int u) {
+        tr[u] = max(tr[u * 2], tr[u * 2 + 1]);
+    }
+};
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        SegmentTree tree(baskets);
+        int n = baskets.size();
+        int ans = 0;
+        for (int x : fruits) {
+            int i = tree.query(1, 1, n, x);
+            if (i < 0) {
+                ans++;
+            } else {
+                tree.modify(1, 1, n, i, 0);
+            }
+        }
+        return ans;
+    }
+};
