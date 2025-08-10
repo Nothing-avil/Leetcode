cbe540d0eb64adeb1006a52b44d7f9fc18c9c273
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-10 22:37:53 +0530

    Create 0869_Reordered_Power_of_2.cpp

---
+++ 0869_Reordered_Power_of_2.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        vector<int> s = convert(n);
+        for (int i = 1; i <= pow(10, 9); i <<= 1)
+            if (s == convert(i))
+                return true;
+        return false;
+    }
+
+    vector<int> convert(int n) {
+        vector<int> cnt(10);
+        for (; n; n /= 10) ++cnt[n % 10];
+        return cnt;
+    }
+};
