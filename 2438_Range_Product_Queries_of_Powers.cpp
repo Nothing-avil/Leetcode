714c9985993ae7943f4f303cd9df3dc49d9dbb50
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-11 23:22:11 +0530

    Create 2438_Range_Product_Queries_of_Powers.cpp

---
+++ 2438_Range_Product_Queries_of_Powers.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> powers;
+        const int mod = 1e9+7;
+        while(n){
+            int t = n & -n;
+            powers.push_back(t);
+            n -= t;
+        }
+        vector<int> answer;
+        for(auto Q : queries){
+            long long ans = 1LL;
+            for(int i = Q[0]; i<= Q[1]; i++){
+                ans = (ans * powers[i]) % mod;
+            }
+            answer.push_back(ans);
+        }
+        return answer;
+    }
+};
