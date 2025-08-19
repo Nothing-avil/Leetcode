d3e5e0bb9258656a4d3fd27bfab59f5ff75a287c
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-19 22:10:49 +0530

    Create 2348_Number_of_Zero-Filled_Subarrays.cpp

---
+++ 2348_Number_of_Zero-Filled_Subarrays.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long answer = 0;
+        long long count = 0;
+        for(int i : nums){
+            if(i ==0){
+                count++;
+            }
+            else if(count != 0){
+                answer += count * (count + 1ll)/2ll;
+                count = 0;
+            }
+        }
+        if(count != 0){
+            answer += count * (count + 1ll)/2ll;
+            count = 0;
+        }
+        return answer;
+    }
+};
