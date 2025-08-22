8a423b23a0653080c811fd0eb3d1ca0953e2cc9a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-22 23:46:07 +0530

    Create 3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.cpp

---
+++ 3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int ai=INT_MAX, bi = INT_MAX, a = 0, b = 0;
+        bool f = true;
+        int n = grid.size(), m = grid[0].size();
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 1){
+                    ai = min(ai, i);
+                    bi = min(bi, j);
+                    a = max(a, i);
+                    b = max(b, j);
+                }
+            }
+        }
+        a = a - ai + 1;
+        b = b - bi + 1;
+        return (a * b);
+    }
+};
