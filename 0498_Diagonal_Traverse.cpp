0a79a1463a757d29725417749d96d396b1a4da07
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-25 22:56:40 +0530

    Create 0498_Diagonal_Traverse.cpp

---
+++ 0498_Diagonal_Traverse.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> ans;
+        vector<int> t;
+        for (int k = 0; k < m + n - 1; ++k) {
+            int i = k < n ? 0 : k - n + 1;
+            int j = k < n ? k : n - 1;
+            while (i < m && j >= 0) t.push_back(mat[i++][j--]);
+            if (k % 2 == 0) reverse(t.begin(), t.end());
+            for (int& v : t) ans.push_back(v);
+            t.clear();
+        }
+        return ans;
+    }
+};
