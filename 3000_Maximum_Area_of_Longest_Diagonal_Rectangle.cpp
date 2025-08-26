a1980f0c6927146101b710a64bda5d1c752a6eb5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-26 23:00:59 +0530

    Create 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.cpp

---
+++ 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        int ares=0;
+        double dis=0;
+        int n= dimensions.size();
+        for(int i=0;i<n; i++){
+            double ar= sqrt(dimensions[i][0]*dimensions[i][0]+ dimensions[i][1]*dimensions[i][1]);
+            int areas= dimensions[i][0]*dimensions[i][1];
+            if(ar>dis|| ar==dis && areas>ares){
+                dis=ar;
+                ares= areas;
+            }
+        }
+        return ares;
+    }
+};
