c40b2c0af8472d972f5da57cb43291b12f4608b8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-31 23:28:00 +0530

    Create 0037_Sudoku_Solver.cpp

---
+++ 0037_Sudoku_Solver.cpp
@@ -0,0 +1,54 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+using pii = pair<int, int>;
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool block[3][3][9] = {false};
+        bool ok = false;
+        vector<pii> t;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    t.push_back({i, j});
+                } else {
+                    int v = board[i][j] - '1';
+                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
+                }
+            }
+        }
+        function<void(int k)> dfs = [&](int k) {
+            if (k == t.size()) {
+                ok = true;
+                return;
+            }
+            int i = t[k].first, j = t[k].second;
+            for (int v = 0; v < 9; ++v) {
+                if (!row[i][v] && !col[j][v] && !block[i / 3][j / 3][v]) {
+                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
+                    board[i][j] = v + '1';
+                    dfs(k + 1);
+                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = false;
+                }
+                if (ok) {
+                    return;
+                }
+            }
+        };
+        dfs(0);
+    }
+};
