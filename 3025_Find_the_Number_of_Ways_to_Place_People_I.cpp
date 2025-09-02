04beb3add8e432aab5a5e6fad6f80141796d09ed
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-02 21:58:29 +0530

    Create 3025_Find_the_Number_of_Ways_to_Place_People_I.cpp

---
+++ 3025_Find_the_Number_of_Ways_to_Place_People_I.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const auto &a, const auto &b) { return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]; });
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; i++){
+            int b = points[i][1], last = -1;
+            for (int j = i + 1; j < n; j++){
+                int a = points[j][1];
+                if (a <= b && a > last){
+                    last = a;
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+};
