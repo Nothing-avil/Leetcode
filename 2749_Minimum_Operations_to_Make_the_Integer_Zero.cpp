46d5e79a41575abfc1ba0394046908be2ec5b88e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-05 23:38:27 +0530

    Create 2749_Minimum_Operations_to_Make_the_Integer_Zero.cpp

---
+++ 2749_Minimum_Operations_to_Make_the_Integer_Zero.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (long long k = 1;; ++k) {
+            long long x = num1 - k * num2;
+            if (x < 0) {
+                break;
+            }
+            if (__builtin_popcountll(x) <= k && k <= x) {
+                return k;
+            }
+        }
+        return -1;
+    }
+};
