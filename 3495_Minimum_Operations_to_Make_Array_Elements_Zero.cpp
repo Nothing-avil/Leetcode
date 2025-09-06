2e0ee9b705efe84227d85f649fd5321f6ead99b6
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-06 23:30:00 +0530

    Create 3495_Minimum_Operations_to_Make_Array_Elements_Zero.cpp

---
+++ 3495_Minimum_Operations_to_Make_Array_Elements_Zero.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minOperations(vector<vector<int>>& queries) {
+        auto f = [&](long long x) {
+            long long res = 0;
+            long long p = 1;
+            int i = 1;
+            while (p <= x) {
+                long long cnt = min(p * 4 - 1, x) - p + 1;
+                res += cnt * i;
+                i++;
+                p *= 4;
+            }
+            return res;
+        };
+
+        long long ans = 0;
+        for (auto& q : queries) {
+            int l = q[0], r = q[1];
+            long long s = f(r) - f(l - 1);
+            long long mx = f(r) - f(r - 1);
+            ans += max((s + 1) / 2, mx);
+        }
+        return ans;
+    }
+};
