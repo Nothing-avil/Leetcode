a6f55623884c1a40cc891bce678467a282dbf77d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-07 22:32:48 +0530

    Create 1304_Find_N_Unique_Integers_Sum_up_to_Zero.cpp

---
+++ 1304_Find_N_Unique_Integers_Sum_up_to_Zero.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> result;
+        if(n % 2 == 1){
+            result.push_back(0);
+            int i = 1;
+            while(result.size() != n){
+                result.push_back(i);
+                result.push_back(-i);
+                i++;
+            }
+        }else{
+            int i = 1;
+            while(result.size() != n){
+                result.push_back(i);
+                result.push_back(-i);
+                i++;
+            }
+        }
+        return result;
+    }
+};
