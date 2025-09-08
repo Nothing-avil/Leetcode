d6e02e1316f211dbec1873c1adbdde0465c04ebf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-08 22:55:02 +0530

    Create 1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.cpp

---
+++ 1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        auto f = [](int x) {
+            for (; x; x /= 10) {
+                if (x % 10 == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        for (int a = 1;; ++a) {
+            int b = n - a;
+            if (f(a) && f(b)) {
+                return {a, b};
+            }
+        }
+    }
+};
