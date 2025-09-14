b6833e62f7cd83d52e9cc8954672365ef9759878
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-14 23:49:38 +0530

    Create 0966_Vowel_Spellchecker.cpp

---
+++ 0966_Vowel_Spellchecker.cpp
@@ -0,0 +1,62 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();class Solution {
+public:
+    vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+        unordered_set<string> s(wordlist.begin(), wordlist.end());
+        unordered_map<string, string> low;
+        unordered_map<string, string> pat;
+        auto f = [](string& w) {
+            string res;
+            for (char& c : w) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    res += '*';
+                } else {
+                    res += c;
+                }
+            }
+            return res;
+        };
+        for (const auto& w : wordlist) {
+            string t = w;
+            transform(t.begin(), t.end(), t.begin(), ::tolower);
+            if (!low.contains(t)) {
+                low[t] = w;
+            }
+            t = f(t);
+            if (!pat.contains(t)) {
+                pat[t] = w;
+            }
+        }
+        vector<string> ans;
+        for (auto& q : queries) {
+            if (s.contains(q)) {
+                ans.emplace_back(q);
+                continue;
+            }
+            transform(q.begin(), q.end(), q.begin(), ::tolower);
+            if (low.contains(q)) {
+                ans.emplace_back(low[q]);
+                continue;
+            }
+            q = f(q);
+            if (pat.contains(q)) {
+                ans.emplace_back(pat[q]);
+                continue;
+            }
+            ans.emplace_back("");
+        }
+        return ans;
+    }
+};
