24fb11a77b51c0ac05503fe9d71be24fc55b52b5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-15 23:19:54 +0530

    Create 1935_Maximum_Number_of_Words_You_Can_Type.cpp

---
+++ 1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        stringstream ss(text);
+        string t;
+        int count = 0;
+        unordered_map<char, int> mp;
+        for(char ch : brokenLetters){
+            mp[ch]++;
+        }
+        function<bool(string)> check = [&](string st){
+            for(char ch : st){
+                if(mp[ch]){
+                    return false;
+                }
+            }
+            return true;
+        };
+        while(ss >> t){
+            if(check(t)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
