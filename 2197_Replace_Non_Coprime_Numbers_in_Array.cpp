19e6226d8137b0b0e11544356e2731ca2d1e4c49
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-16 23:09:59 +0530

    Create 2197_Replace_Non_Coprime_Numbers_in_Array.cpp

---
+++ 2197_Replace_Non_Coprime_Numbers_in_Array.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> stk;
+        for (int x : nums) {
+            stk.push_back(x);
+            while (stk.size() > 1) {
+                x = stk.back();
+                int y = stk[stk.size() - 2];
+                int g = __gcd(x, y);
+                if (g == 1) {
+                    break;
+                }
+                stk.pop_back();
+                stk.back() = 1LL * x * y / g;
+            }
+        }
+        return stk;
+    }
+};
