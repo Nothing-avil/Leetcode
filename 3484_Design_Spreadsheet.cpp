513d50eb403af6e3879a8794d4f8cb74461afa5f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-09-20 00:04:28 +0530

    Create 3484_Design_Spreadsheet.cpp

---
+++ 3484_Design_Spreadsheet.cpp
@@ -0,0 +1,51 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Spreadsheet {
+private:
+    unordered_map<string, int> d;
+
+public:
+    Spreadsheet(int rows) {}
+
+    void setCell(string cell, int value) {
+        d[cell] = value;
+    }
+
+    void resetCell(string cell) {
+        d.erase(cell);
+    }
+
+    int getValue(string formula) {
+        int ans = 0;
+        stringstream ss(formula.substr(1));
+        string cell;
+        while (getline(ss, cell, '+')) {
+            if (isdigit(cell[0])) {
+                ans += stoi(cell);
+            } else {
+                ans += d.count(cell) ? d[cell] : 0;
+            }
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
