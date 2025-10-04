9048adb89b239a94d05a98e0fc84ee0a1eb8eb7e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-04 22:29:13 +0530

    Create 0011_Container_With_Most_Water.cpp

---
+++ 0011_Container_With_Most_Water.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int width=height.size()-1;
+        int ptr1=0;
+        int ptr2=height.size()-1;
+        int ans=0;
+        while(ptr1<ptr2){
+            int h=min(height[ptr1],height[ptr2]);
+            ans=max(ans,h*width);
+            width--;
+            if(height[ptr1]>height[ptr2]){
+                ptr2--;
+            }
+            else{
+                ptr1++;
+            }
+        }
+        return ans;
+    }
+};
