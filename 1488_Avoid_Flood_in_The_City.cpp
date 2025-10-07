5e4eb754abf8c11bf49ec225abbf9c5228776fb6
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-07 22:47:40 +0530

    Create 1488_Avoid_Flood_in_The_City.cpp

---
+++ 1488_Avoid_Flood_in_The_City.cpp
@@ -0,0 +1,41 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int> ans(n, -1);
+        set<int> sunny;
+        unordered_map<int, int> rainy;
+        for (int i = 0; i < n; ++i) {
+            int v = rains[i];
+            if (v) {
+                if (rainy.count(v)) {
+                    auto it = sunny.upper_bound(rainy[v]);
+                    if (it == sunny.end()) {
+                        return {};
+                    }
+                    ans[*it] = v;
+                    sunny.erase(it);
+                }
+                rainy[v] = i;
+            } else {
+                sunny.insert(i);
+                ans[i] = 1;
+            }
+        }
+        return ans;
+    }
+};
