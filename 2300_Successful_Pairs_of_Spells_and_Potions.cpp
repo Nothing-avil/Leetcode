5e5bb3035e2beac97fcfab167e8d55e967961bce
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-08 22:30:30 +0530

    Create 2300_Successful_Pairs_of_Spells_and_Potions.cpp

---
+++ 2300_Successful_Pairs_of_Spells_and_Potions.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin (), potions.end());
+        vector<int>ans;
+        int m = potions.size();
+        for (int spell : spells){
+            long long minPotion = (success + spell -1) / spell;
+            int low = 0;
+            int high = m-1;
+            int firstValid = m;
+            while(low<=high){
+                int mid = low + (high -low)/2;
+                if((long long) potions[mid] >= minPotion){
+                    firstValid = mid;
+                    high = mid-1;
+                }else {
+                    low = mid+1;
+                }
+            }
+            int count = m - firstValid;
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
