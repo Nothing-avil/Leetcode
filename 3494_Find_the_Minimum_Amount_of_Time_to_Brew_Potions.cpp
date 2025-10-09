8c42511af66fee4ade3f0b5f7852e505acec2b38
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-09 22:14:00 +0530

    Create 3494_Find_the_Minimum_Amount_of_Time_to_Brew_Potions.cpp

---
+++ 3494_Find_the_Minimum_Amount_of_Time_to_Brew_Potions.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        int n = skill.size();
+        vector<long long> f(n);
+        for (int x : mana) {
+            long long tot = 0;
+            for (int i = 0; i < n; ++i) {
+                tot = max(tot, f[i]) + 1LL * skill[i] * x;
+            }
+            f[n - 1] = tot;
+            for (int i = n - 2; i >= 0; --i) {
+                f[i] = f[i + 1] - 1LL * skill[i + 1] * x;
+            }
+        }
+        return f[n - 1];
+    }
+};
