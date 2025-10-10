f5a066ace7bcd807aad6901e82b7d7ed92e8cacc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-10 22:47:50 +0530

    Create 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.cpp

---
+++ 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        vector<int> dp(energy.size(),-1);
+        int ans=INT_MIN;
+        for(int i=energy.size()-1;i>=0;i--){
+            if(i+k<energy.size()){
+                dp[i]=energy[i]+dp[i+k];
+            }
+            else{
+                dp[i]=energy[i];
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
