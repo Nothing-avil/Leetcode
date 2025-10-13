9fd770129ca222725db4daa4e25577b08cac073a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-13 22:46:35 +0530

    Create 2273_Find_Resultant_Array_After_Removing_Anagrams.cpp

---
+++ 2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
@@ -0,0 +1,41 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        auto check = [](string& s, string& t) -> bool {
+            if (s.size() != t.size()) {
+                return true;
+            }
+            int cnt[26]{};
+            for (char& c : s) {
+                ++cnt[c - 'a'];
+            }
+            for (char& c : t) {
+                if (--cnt[c - 'a'] < 0) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        vector<string> ans = {words[0]};
+        for (int i = 1; i < words.size(); ++i) {
+            if (check(words[i - 1], words[i])) {
+                ans.emplace_back(words[i]);
+            }
+        }
+        return ans;
+    }
+};
