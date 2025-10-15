62e5527f509d393f2c060ae8302ebee3ea0e86cc
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-15 22:32:30 +0530

    Create 3350_Adjacent_Increasing_Subarrays_Detection_II.cpp

---
+++ 3350_Adjacent_Increasing_Subarrays_Detection_II.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int ans = 0, pre = 0, cur = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            ++cur;
+            if (i == n - 1 || nums[i] >= nums[i + 1]) {
+                ans = max({ans, cur / 2, min(pre, cur)});
+                pre = cur;
+                cur = 0;
+            }
+        }
+        return ans;
+    }
+};
