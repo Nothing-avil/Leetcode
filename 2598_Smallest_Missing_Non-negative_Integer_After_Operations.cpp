3e8d0f2ea7b6c502dee2a273bfdc5d08dcf112cf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-16 23:33:17 +0530

    Create 2598_Smallest_Missing_Non-negative_Integer_After_Operations.cpp

---
+++ 2598_Smallest_Missing_Non-negative_Integer_After_Operations.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        int cnt[value];
+        memset(cnt, 0, sizeof(cnt));
+        for (int x : nums) {
+            ++cnt[(x % value + value) % value];
+        }
+        for (int i = 0;; ++i) {
+            if (cnt[i % value]-- == 0) {
+                return i;
+            }
+        }
+    }
+};
