52a0281718481986ee9ffcfe556a4a8cd20e44a5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-18 23:12:12 +0530

    Create 3397_Maximum_Number_of_Distinct_Elements_After_Operations.cpp

---
+++ 3397_Maximum_Number_of_Distinct_Elements_After_Operations.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        ranges::sort(nums);
+        int ans = 0, pre = INT_MIN;
+        for (int x : nums) {
+            int cur = min(x + k, max(x - k, pre + 1));
+            if (cur > pre) {
+                ++ans;
+                pre = cur;
+            }
+        }
+        return ans;
+    }
+};
