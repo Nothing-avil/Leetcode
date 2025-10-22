8789c217ae8f39980707464e0fe5c901b7d4f705
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-22 23:41:53 +0530

    Create 3347_Maximum_Frequency_of_an_Element_After_Performing_Operations_II.cpp

---
+++ 3347_Maximum_Frequency_of_an_Element_After_Performing_Operations_II.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        unordered_map<int, int> cnt;
+        map<int, int> d;
+        for (int x : nums) {
+            cnt[x]++;
+            d[x];
+            d[x - k]++;
+            d[x + k + 1]--;
+        }
+        int ans = 0, s = 0;
+        for (const auto& [x, t] : d) {
+            s += t;
+            ans = max(ans, min(s, cnt[x] + numOperations));
+        }
+        return ans;
+    }
+};
