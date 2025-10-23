486f3b7501963e128d2fa2b3ab629918e2e714a2
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-23 22:58:17 +0530

    Create 3461_Check_If_Digits_Are_Equal_in_String_After_Operations_I.cpp

---
+++ 3461_Check_If_Digits_Are_Equal_in_String_After_Operations_I.cpp
@@ -0,0 +1,27 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int n = s.size();
+        string t = s;
+        for (int k = n - 1; k > 1; --k) {
+            for (int i = 0; i < k; ++i) {
+                t[i] = (t[i] - '0' + t[i + 1] - '0') % 10 + '0';
+            }
+        }
+        return t[0] == t[1];
+    }
+};
