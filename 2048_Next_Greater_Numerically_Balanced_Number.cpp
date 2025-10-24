6b9d01f01053a480430171df912d415aa10e1072
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-24 23:21:23 +0530

    Create 2048_Next_Greater_Numerically_Balanced_Number.cpp

---
+++ 2048_Next_Greater_Numerically_Balanced_Number.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        for (int x = n + 1;; ++x) {
+            int cnt[10]{};
+            for (int y = x; y > 0; y /= 10) {
+                ++cnt[y % 10];
+            }
+            bool ok = true;
+            for (int y = x; y > 0; y /= 10) {
+                if (y % 10 != cnt[y % 10]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                return x;
+            }
+        }
+    }
+};
