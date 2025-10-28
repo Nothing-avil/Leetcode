dc6a3f9f99cb22938645ab3b7450685aedb9a427
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-10-28 22:18:42 +0530

    Create 3354_Make_Array_Elements_Equal_to_Zero.cpp

---
+++ 3354_Make_Array_Elements_Equal_to_Zero.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        int ans = 0, l = 0;
+        for (int x : nums) {
+            if (x) {
+                l += x;
+            } else if (l * 2 == s) {
+                ans += 2;
+            } else if (abs(l * 2 - s) <= 1) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
