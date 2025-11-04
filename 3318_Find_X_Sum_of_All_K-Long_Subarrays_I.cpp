23db71f0625d5ba6b99019631efda70b8be8b44f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-04 22:18:34 +0530

    Create 3318_Find_X_Sum_of_All_K-Long_Subarrays_I.cpp

---
+++ 3318_Find_X_Sum_of_All_K-Long_Subarrays_I.cpp
@@ -0,0 +1,75 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        using pii = pair<int, int>;
+        set<pii> l, r;
+        int s = 0;
+        unordered_map<int, int> cnt;
+        auto add = [&](int v) {
+            if (cnt[v] == 0) {
+                return;
+            }
+            pii p = {cnt[v], v};
+            if (!l.empty() && p > *l.begin()) {
+                s += p.first * p.second;
+                l.insert(p);
+            } else {
+                r.insert(p);
+            }
+        };
+        auto remove = [&](int v) {
+            if (cnt[v] == 0) {
+                return;
+            }
+            pii p = {cnt[v], v};
+            auto it = l.find(p);
+            if (it != l.end()) {
+                s -= p.first * p.second;
+                l.erase(it);
+            } else {
+                r.erase(p);
+            }
+        };
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            remove(nums[i]);
+            ++cnt[nums[i]];
+            add(nums[i]);
+            int j = i - k + 1;
+            if (j < 0) {
+                continue;
+            }
+            while (!r.empty() && l.size() < x) {
+                pii p = *r.rbegin();
+                s += p.first * p.second;
+                r.erase(p);
+                l.insert(p);
+            }
+            while (l.size() > x) {
+                pii p = *l.begin();
+                s -= p.first * p.second;
+                l.erase(p);
+                r.insert(p);
+            }
+            ans.push_back(s);
+            remove(nums[j]);
+            --cnt[nums[j]];
+            add(nums[j]);
+        }
+        return ans;
+    }
+};
