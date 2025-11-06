94bc2cad80fa865fb81602c0fd81c5c1548ce1c0
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-06 22:48:12 +0530

    Create 3607_Power_Grid_Maintenance.cpp

---
+++ 3607_Power_Grid_Maintenance.cpp
@@ -0,0 +1,76 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class UnionFind {
+public:
+    UnionFind(int n) {
+        p = vector<int>(n);
+        size = vector<int>(n, 1);
+        iota(p.begin(), p.end(), 0);
+    }
+    bool unite(int a, int b) {
+        int pa = find(a), pb = find(b);
+        if (pa == pb) {
+            return false;
+        }
+        if (size[pa] > size[pb]) {
+            p[pb] = pa;
+            size[pa] += size[pb];
+        } else {
+            p[pa] = pb;
+            size[pb] += size[pa];
+        }
+        return true;
+    }
+    int find(int x) {
+        if (p[x] != x) {
+            p[x] = find(p[x]);
+        }
+        return p[x];
+    }
+private:
+    vector<int> p, size;
+};
+
+class Solution {
+public:
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        UnionFind uf(c + 1);
+        for (auto& e : connections) {
+            uf.unite(e[0], e[1]);
+        }
+
+        vector<set<int>> st(c + 1);
+        for (int i = 1; i <= c; i++) {
+            st[uf.find(i)].insert(i);
+        }
+        vector<int> ans;
+        for (auto& q : queries) {
+            int a = q[0], x = q[1];
+            int root = uf.find(x);
+            if (a == 1) {
+                if (st[root].count(x)) {
+                    ans.push_back(x);
+                } else if (!st[root].empty()) {
+                    ans.push_back(*st[root].begin());
+                } else {
+                    ans.push_back(-1);
+                }
+            } else {
+                st[root].erase(x);
+            }
+        }
+        return ans;
+    }
+};
