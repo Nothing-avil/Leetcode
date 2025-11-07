46a96653aa4a7f8b245403b999b343a82547f700
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-07 22:58:16 +0530

    Create 2528_Maximize_the_Minimum_Powered_City.cpp

---
+++ 2528_Maximize_the_Minimum_Powered_City.cpp
@@ -0,0 +1,62 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long maxPower(vector<int>& stations, int r, int k) {
+        int n = stations.size();
+        long long d[n + 1];
+        memset(d, 0, sizeof d);
+        for (int i = 0; i < n; ++i) {
+            int left = max(0, i - r), right = min(i + r, n - 1);
+            d[left] += stations[i];
+            d[right + 1] -= stations[i];
+        }
+        long long s[n + 1];
+        s[0] = d[0];
+        for (int i = 1; i < n + 1; ++i) {
+            s[i] = s[i - 1] + d[i];
+        }
+        auto check = [&](long long x, int k) {
+            memset(d, 0, sizeof d);
+            long long t = 0;
+            for (int i = 0; i < n; ++i) {
+                t += d[i];
+                long long dist = x - (s[i] + t);
+                if (dist > 0) {
+                    if (k < dist) {
+                        return false;
+                    }
+                    k -= dist;
+                    int j = min(i + r, n - 1);
+                    int left = max(0, j - r), right = min(j + r, n - 1);
+                    d[left] += dist;
+                    d[right + 1] -= dist;
+                    t += dist;
+                }
+            }
+            return true;
+        };
+        long long left = 0, right = 1e12;
+        while (left < right) {
+            long long mid = (left + right + 1) >> 1;
+            if (check(mid, k)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
