4b69cdfe133d1f7da1a12f5093045a9290fa12c8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-10 23:17:12 +0530

    Create 3542_Minimum_Operations_to_Convert_All_Elements_to_Zero.cpp

---
+++ 3542_Minimum_Operations_to_Convert_All_Elements_to_Zero.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        vector<int> stk;
+        int ans = 0;
+        for (int x : nums) {
+            while (!stk.empty() && stk.back() > x) {
+                ++ans;
+                stk.pop_back();
+            }
+            if (x != 0 && (stk.empty() || stk.back() != x)) {
+                stk.push_back(x);
+            }
+        }
+        ans += stk.size();
+        return ans;
+    }
+};
