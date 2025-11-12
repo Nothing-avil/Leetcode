c9eb9a0495ea3a7218f24085d52993cdc6448d59
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-12 23:27:25 +0530

    Create 2654_Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.cpp

---
+++ 2654_Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.cpp
@@ -0,0 +1,40 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                ++cnt;
+            }
+        }
+        if (cnt) {
+            return n - cnt;
+        }
+        int mi = n + 1;
+        for (int i = 0; i < n; ++i) {
+            int g = 0;
+            for (int j = i; j < n; ++j) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    mi = min(mi, j - i + 1);
+                }
+            }
+        }
+        return mi > n ? -1 : n - 1 + mi - 1;
+    }
+};
