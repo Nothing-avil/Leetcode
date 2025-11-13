4480dc43da49269836514db63c933c1140fa349d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-13 23:48:50 +0530

    Create 3228_Maximum_Number_of_Operations_to_Move_Ones_to_the_End.cpp

---
+++ 3228_Maximum_Number_of_Operations_to_Move_Ones_to_the_End.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxOperations(string s) {
+        int ans = 0, cnt = 0;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '1') {
+                ++cnt;
+            } else if (i && s[i - 1] == '1') {
+                ans += cnt;
+            }
+        }
+        return ans;
+    }
+};
