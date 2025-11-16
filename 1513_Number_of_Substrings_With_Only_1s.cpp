229174b2a1bb1de9f3eacd8c79bbb4a0b916565f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-16 23:06:14 +0530

    Create 1513_Number_of_Substrings_With_Only_1s.cpp

---
+++ 1513_Number_of_Substrings_With_Only_1s.cpp
@@ -0,0 +1,26 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int numSub(string s) {
+        int ans = 0, cnt = 0;
+        const int mod = 1e9 + 7;
+        for (char& c : s) {
+            cnt = c == '1' ? cnt + 1 : 0;
+            ans = (ans + cnt) % mod;
+        }
+        return ans;
+    }
+};
