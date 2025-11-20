b8e74239fdc553ba90a83c14551e0b8586861cc5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-20 23:31:14 +0530

    Create 0757_Set_Intersection_Size_At_Least_Two.cpp

---
+++ 0757_Set_Intersection_Size_At_Least_Two.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [&](vector<int>& a, vector<int>& b) {
+            return a[1] == b[1] ? a[0] > b[0] : a[1] < b[1];
+        });
+        int ans = 0;
+        int s = -1, e = -1;
+        for (auto& v : intervals) {
+            int a = v[0], b = v[1];
+            if (a <= s) continue;
+            if (a > e) {
+                ans += 2;
+                s = b - 1;
+                e = b;
+            } else {
+                ans += 1;
+                s = e;
+                e = b;
+            }
+        }
+        return ans;
+    }
+};
