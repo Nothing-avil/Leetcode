816ae67163ef879029c126f899b26ea6b3506be9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-22 16:02:36 +0530

    Create 3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.cpp

---
+++ 3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.cpp
@@ -0,0 +1,27 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ans = 0;
+        for (int x : nums) {
+            int mod = x % 3;
+            if (mod) {
+                ans += min(mod, 3 - mod);
+            }
+        }
+        return ans;
+    }
+};
