b843e627b6785f38b73c93138372b9f817210946
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-02 22:16:52 +0530

    Create 3623_Count_Number_of_Trapezoids_I.cpp

---
+++ 3623_Count_Number_of_Trapezoids_I.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        const int mod = 1e9 + 7;
+        unordered_map<int, int> cnt;
+        for (auto& p : points) {
+            cnt[p[1]]++;
+        }
+        long long ans = 0, s = 0;
+        for (auto& [_, v] : cnt) {
+            long long t = 1LL * v * (v - 1) / 2;
+            ans = (ans + s * t) % mod;
+            s += t;
+        }
+        return (int) ans;
+    }
+};
