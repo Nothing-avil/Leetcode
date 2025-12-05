80dc249e5d1fbc0284f5acbc9f9bb5ee8c90b459
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-05 23:09:09 +0530

    Create 3432_Count_Partitions_with_Even_Sum_Difference.cpp

---
+++ 3432_Count_Partitions_with_Even_Sum_Difference.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int l = 0, r = accumulate(nums.begin(), nums.end(), 0);
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            l += nums[i];
+            r -= nums[i];
+            if ((l - r) % 2 == 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
