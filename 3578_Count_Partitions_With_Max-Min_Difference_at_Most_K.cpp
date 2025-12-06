0260e6859b998a5244734c6606744e2d0cd51e78
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-06 22:50:57 +0530

    Create 3578_Count_Partitions_With_Max-Min_Difference_at_Most_K.cpp

---
+++ 3578_Count_Partitions_With_Max-Min_Difference_at_Most_K.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int k) {
+        const int mod = 1e9 + 7;
+        multiset<int> sl;
+        int n = nums.size();
+        vector<int> f(n + 1, 0), g(n + 1, 0);
+        f[0] = 1;
+        g[0] = 1;
+        int l = 1;
+        for (int r = 1; r <= n; ++r) {
+            int x = nums[r - 1];
+            sl.insert(x);
+            while (*sl.rbegin() - *sl.begin() > k) {
+                sl.erase(sl.find(nums[l - 1]));
+                ++l;
+            }
+            f[r] = (g[r - 1] - (l >= 2 ? g[l - 2] : 0) + mod) % mod;
+            g[r] = (g[r - 1] + f[r]) % mod;
+        }
+        return f[n];
+    }
+};
