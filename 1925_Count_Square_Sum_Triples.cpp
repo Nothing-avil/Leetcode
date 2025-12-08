16f1abcd9f69f8329f6ec09e5cd052b9a9887123
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-08 21:32:28 +0530

    Create 1925_Count_Square_Sum_Triples.cpp

---
+++ 1925_Count_Square_Sum_Triples.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        for (int a = 1; a < n; ++a) {
+            for (int b = 1; b < n; ++b) {
+                int x = a * a + b * b;
+                int c = static_cast<int>(sqrt(x));
+                if (c <= n && c * c == x) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
