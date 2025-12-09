8f933469936884c90ca3ea212b312a37c824a335
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-09 22:00:58 +0530

    Create 3583_Count_Special_Triplets.cpp

---
+++ 3583_Count_Special_Triplets.cpp
@@ -0,0 +1,31 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        unordered_map<int, int> left, right;
+        for (int x : nums) {
+            right[x]++;
+        }
+        long long ans = 0;
+        const int mod = 1e9 + 7;
+        for (int x : nums) {
+            right[x]--;
+            ans = (ans + 1LL * left[x * 2] * right[x * 2] % mod) % mod;
+            left[x]++;
+        }
+        return (int) ans;
+    }
+};
