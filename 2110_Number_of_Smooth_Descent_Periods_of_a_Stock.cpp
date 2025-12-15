91464f9faf219979180c8c577f594c9623597299
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-15 23:01:41 +0530

    Create 2110_Number_of_Smooth_Descent_Periods_of_a_Stock.cpp

---
+++ 2110_Number_of_Smooth_Descent_Periods_of_a_Stock.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0;
+        int n = prices.size();
+        for (int i = 0, j = 0; i < n; i = j) {
+            j = i + 1;
+            while (j < n && prices[j - 1] - prices[j] == 1) {
+                ++j;
+            }
+            int cnt = j - i;
+            ans += (1LL + cnt) * cnt / 2;
+        }
+        return ans;
+    }
+};
