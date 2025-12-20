37d0060b24d62a1d1e9d243ededca6bc77e31a13
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-20 23:34:19 +0530

    Create 0944_Delete_Columns_to_Make_Sorted.cpp

---
+++ 0944_Delete_Columns_to_Make_Sorted.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int m = strs[0].size(), n = strs.size();
+        int ans = 0;
+        for (int j = 0; j < m; ++j) {
+            for (int i = 1; i < n; ++i) {
+                if (strs[i][j] < strs[i - 1][j]) {
+                    ++ans;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
