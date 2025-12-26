c64caab75eb309c067dbc67fecef895f1e87f408
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-26 22:40:19 +0530

    Create 2483_Minimum_Penalty_for_a_Shop.cpp

---
+++ 2483_Minimum_Penalty_for_a_Shop.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int ans = 0;
+        int cost = 0;
+        for (char c : customers) {
+            if (c == 'Y') {
+                cost++;
+            }
+        }
+        int mn = cost;
+        for (int j = 1; j <= customers.size(); ++j) {
+            cost += customers[j - 1] == 'N' ? 1 : -1;
+            if (cost < mn) {
+                ans = j;
+                mn = cost;
+            }
+        }
+        return ans;
+    }
+};
