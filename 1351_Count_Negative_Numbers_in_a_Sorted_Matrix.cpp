ef9917bc69e0b49696b5afad8df677ed55b15978
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-28 20:49:49 +0530

    Create 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp

---
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int i = 0, j = m -1;
+        int answer = 0;
+        while(i < n && j >=0){
+            if(grid[i][j] >= 0){
+                i++;
+            }
+            else{
+                answer += (n - i);
+                j--;
+            }
+        }
+        return answer;
+    }
+};
