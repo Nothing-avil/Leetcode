89addaafebc5e9d5b283af445cc7be15e7fe4dfb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-07 00:11:10 +0530

    Create 1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp

---
+++ 1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        vector<int> s;
+        dfs(root, 0, s);
+        int mx = INT_MIN;
+        int ans = 0;
+        for (int i = 0; i < s.size(); ++i)
+            if (mx < s[i]) mx = s[i], ans = i + 1;
+        return ans;
+    }
+
+    void dfs(TreeNode* root, int i, vector<int>& s) {
+        if (!root) return;
+        if (s.size() == i)
+            s.push_back(root->val);
+        else
+            s[i] += root->val;
+        dfs(root->left, i + 1, s);
+        dfs(root->right, i + 1, s);
+    }
+};
