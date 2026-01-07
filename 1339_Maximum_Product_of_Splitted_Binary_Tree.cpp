8377c8ddbca477afc0da7d835063f677594a4f8f
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-07 22:50:25 +0530

    Create 1339_Maximum_Product_of_Splitted_Binary_Tree.cpp

---
+++ 1339_Maximum_Product_of_Splitted_Binary_Tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxProduct(TreeNode* root) {
+        using ll = long long;
+        ll ans = 0;
+        const int mod = 1e9 + 7;
+        function<ll(TreeNode*)> sum = [&](TreeNode* root) -> ll {
+            if (!root) {
+                return 0;
+            }
+            return root->val + sum(root->left) + sum(root->right);
+        };
+        ll s = sum(root);
+        function<ll(TreeNode*)> dfs = [&](TreeNode* root) -> ll {
+            if (!root) {
+                return 0;
+            }
+            ll t = root->val + dfs(root->left) + dfs(root->right);
+            if (t < s) {
+                ans = max(ans, t * (s - t));
+            }
+            return t;
+        };
+        dfs(root);
+        return ans % mod;
+    }
+};
