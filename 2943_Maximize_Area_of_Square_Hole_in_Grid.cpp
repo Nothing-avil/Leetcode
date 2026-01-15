c8b34b161376e00cba5a7e1cf79a565587d2b0f9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-15 22:35:59 +0530

    Create 2943_Maximize_Area_of_Square_Hole_in_Grid.cpp

---
+++ 2943_Maximize_Area_of_Square_Hole_in_Grid.cpp
@@ -0,0 +1,34 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        auto f = [](vector<int>& nums) {
+            int ans = 1, cnt = 1;
+            sort(nums.begin(), nums.end());
+            for (int i = 1; i < nums.size(); ++i) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    ans = max(ans, ++cnt);
+                } else {
+                    cnt = 1;
+                }
+            }
+            return ans + 1;
+        };
+        int x = min(f(hBars), f(vBars));
+        return x * x;
+    }
+};
