77ffbbda7d1d3167eae7198343f624fd1e2760fb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-16 22:58:09 +0530

    Create 2975_Maximum_Square_Area_by_Removing_Fences_From_a_Field.cpp

---
+++ 2975_Maximum_Square_Area_by_Removing_Fences_From_a_Field.cpp
@@ -0,0 +1,45 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        hFences.push_back(1);
+        hFences.push_back(m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+        sort(hFences.begin(), hFences.end());
+        sort(vFences.begin(), vFences.end());
+        unordered_set<int> len;
+        long long e = -1;
+        for (int i = 0; i < hFences.size(); ++i) {
+            for (int j = i + 1; j < hFences.size(); ++j) {
+                len.insert(hFences[j] - hFences[i]);
+            }
+        }
+        for (int i = 0; i < vFences.size(); ++i) {
+            for (int j = i + 1; j < vFences.size(); ++j) {
+                int l = vFences[j] - vFences[i];
+                if (len.count(l)) {
+                    e = max(e, (long long) l);
+                }
+            }
+        }
+        if (e == -1) {
+            return -1;
+        }
+        return e * e % 1000000007;
+    }
+};
