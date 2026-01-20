9ee9aa352dbf2563664084ee540b660fb4e15af7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-21 00:10:56 +0530

    Create 3314_Construct_the_Minimum_Bitwise_Array_I.cpp

---
+++ 3314_Construct_the_Minimum_Bitwise_Array_I.cpp
@@ -0,0 +1,35 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int i : nums){
+            bool check = true;
+            for(int j=0; j<=i; j++){
+                if(((j) | (j + 1)) == i){
+                    ans.push_back(j);
+                    check = false;
+                    break;
+                }
+            }
+            if(check){
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
