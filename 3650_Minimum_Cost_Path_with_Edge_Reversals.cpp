e53ec3801783dd7c5887742130bae83b5daaf0cd
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-01-27 23:10:15 +0530

    Create 3650_Minimum_Cost_Path_with_Edge_Reversals.cpp

---
+++ 3650_Minimum_Cost_Path_with_Edge_Reversals.cpp
@@ -0,0 +1,50 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        using pii = pair<int, int>;
+        vector<vector<pii>> g(n);
+        for (auto& e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            g[u].push_back({v, w});
+            g[v].push_back({u, w * 2});
+        }
+        const int inf = INT_MAX / 2;
+        vector<int> dist(n, inf);
+        dist[0] = 0;
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        pq.push({0, 0});
+        while (!pq.empty()) {
+            auto [d, u] = pq.top();
+            pq.pop();
+            if (d > dist[u]) {
+                continue;
+            }
+            if (u == n - 1) {
+                return d;
+            }
+            for (auto& [v, w] : g[u]) {
+                int nd = d + w;
+                if (nd < dist[v]) {
+                    dist[v] = nd;
+                    pq.push({nd, v});
+                }
+            }
+        }
+        return -1;
+    }
+};
