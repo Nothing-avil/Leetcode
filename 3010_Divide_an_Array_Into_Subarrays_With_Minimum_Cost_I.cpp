6c52deb3d492bbb47f11826e44b153e299ff8675
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-01 21:57:58 +0530

    Create 3010_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I.cpp

---
+++ 3010_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int m1 = INT_MAX, m2 = INT_MAX;
+        for (int i = 1;i < nums.size();++i) {
+            int x = nums[i];
+            if (x < m1) {
+                m2 = m1;
+                m1 = x;
+            }
+            else if (x < m2) {
+                m2 = x;
+            }
+        }
+        return nums[0] + m1 + m2;
+    }
+};
