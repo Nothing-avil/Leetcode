a002e53b5b37efd7fd3fe21d79d84a04ccf08eb6
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-12 23:08:23 +0530

    Create 3713_Longest_Balanced_Substring_I.cpp

---
+++ 3713_Longest_Balanced_Substring_I.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+};
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        vector<int> cnt(26, 0);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            fill(cnt.begin(), cnt.end(), 0);
+            int mx = 0, v = 0;
+            for (int j = i; j < n; ++j) {
+                int c = s[j] - 'a';
+                if (++cnt[c] == 1) {
+                    ++v;
+                }
+                mx = max(mx, cnt[c]);
+                if (mx * v == j - i + 1) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
