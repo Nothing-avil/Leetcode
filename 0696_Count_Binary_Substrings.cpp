8ab686b6cbe933aa4f9176df7c48c810c5542cbb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-19 22:11:28 +0530

    Create 0696_Count_Binary_Substrings.cpp

---
+++ 0696_Count_Binary_Substrings.cpp
@@ -0,0 +1,33 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+};
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int i = 0, n = s.size();
+        vector<int> t;
+        while (i < n) {
+            int cnt = 1;
+            while (i + 1 < n && s[i + 1] == s[i]) {
+                ++cnt;
+                ++i;
+            }
+            t.push_back(cnt);
+            ++i;
+        }
+        int ans = 0;
+        for (i = 1; i < t.size(); ++i) ans += min(t[i - 1], t[i]);
+        return ans;
+    }
+};
