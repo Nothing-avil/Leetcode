a73c46f6923b7f6118a7f94522e7f71147604e2e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-21 21:35:30 +0530

    Create 0762_Prime_Number_of_Set_Bits_in_Binary_Representation.cpp

---
+++ 0762_Prime_Number_of_Set_Bits_in_Binary_Representation.cpp
@@ -0,0 +1,25 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+};
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes{2, 3, 5, 7, 11, 13, 17, 19};
+        int ans = 0;
+        for (int i = left; i <= right; ++i) {
+            ans += primes.count(__builtin_popcount(i));
+        }
+        return ans;
+    }
+};
