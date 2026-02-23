b9751421b6b9815822aa5ce60822ba353717bef5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-23 23:22:18 +0530

    Create 1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.cpp

---
+++ 1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.cpp
@@ -0,0 +1,29 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+};
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        int n = s.size();
+        int checker = 1 << k;
+        if(n - k + 1 < checker){
+            return false;
+        }
+        unordered_set<string> st;
+        for(int i=0; i+k <= n; i++){
+            st.insert(move(s.substr(i, k)));
+        }
+        return st.size() == checker;
+    }
+};
