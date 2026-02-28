82102cd2eef18acd6b3747ac0ca17093ba3e9c22
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-28 23:51:38 +0530

    Create 1680_Concatenation_of_Consecutive_Binary_Numbers.cpp

---
+++ 1680_Concatenation_of_Consecutive_Binary_Numbers.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int mod = 1e9+7;
+        long ans = 0, shift = 0;
+        for(int i=1; i<=n; i++){
+            if((i & (i-1)) == 0){
+                shift++;
+            }
+            ans = (ans << shift | i) % mod;
+        }
+        return ans;
+    }
+};
