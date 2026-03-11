d515692ca1a4f05840085ca12949a8f9e504ac1e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-03-11 22:35:40 +0530

    Create 1009_Complement_of_Base_10_Integer.cpp

---
+++ 1009_Complement_of_Base_10_Integer.cpp
@@ -0,0 +1,32 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int bitwiseComplement(int num) {
+        if(num==0){
+            return 1;
+        }
+        int ni=num;
+        int c=0;
+        while(ni>0){
+            ni= ni>>1;
+            c++;
+        }
+        ni = (1 << c) - 1;
+        return (num ^ ni);
+    }
+
+};
