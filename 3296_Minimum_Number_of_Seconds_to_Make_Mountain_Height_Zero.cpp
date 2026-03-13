a78299d8e72bd2f2abfaa41b6fa41f0118856dbb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-03-13 22:24:18 +0530

    Create 3296_Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero.cpp

---
+++ 3296_Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero.cpp
@@ -0,0 +1,37 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long l = 1, r = 1e16;
+        auto check = [&](long long t) -> bool {
+            long long h = 0;
+            for (int& wt : workerTimes) {
+                h += (long long) (sqrt(t * 2.0 / wt + 0.25) - 0.5);
+            }
+            return h >= mountainHeight;
+        };
+        while (l < r) {
+            long long mid = (l + r) >> 1;
+            if (check(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
