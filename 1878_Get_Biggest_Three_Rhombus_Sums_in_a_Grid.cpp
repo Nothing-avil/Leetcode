61c5f8d441beecdc2c64945ee5b4eaf737588385
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-03-16 22:51:06 +0530

    Create 1878_Get_Biggest_Three_Rhombus_Sums_in_a_Grid.cpp

---
+++ 1878_Get_Biggest_Three_Rhombus_Sums_in_a_Grid.cpp
@@ -0,0 +1,47 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#include <ranges>
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> s1(m + 1, vector<int>(n + 2));
+        vector<vector<int>> s2(m + 1, vector<int>(n + 2));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                s1[i][j] = s1[i - 1][j - 1] + grid[i - 1][j - 1];
+                s2[i][j] = s2[i - 1][j + 1] + grid[i - 1][j - 1];
+            }
+        }
+        set<int> ss;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                int l = min({i - 1, m - i, j - 1, n - j});
+                ss.insert(grid[i - 1][j - 1]);
+                for (int k = 1; k <= l; ++k) {
+                    int a = s1[i + k][j] - s1[i][j - k];
+                    int b = s1[i][j + k] - s1[i - k][j];
+                    int c = s2[i][j - k] - s2[i - k][j];
+                    int d = s2[i + k][j] - s2[i][j + k];
+                    ss.insert(a + b + c + d - grid[i + k - 1][j - 1] + grid[i - k - 1][j - 1]);
+                }
+                while (ss.size() > 3) {
+                    ss.erase(ss.begin());
+                }
+            }
+        }
+        return vector<int>(ss.rbegin(), ss.rend());
+    }
+};
