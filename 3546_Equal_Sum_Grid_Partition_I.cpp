fb3d8983568ba59035e7947c6fadb71c588e4ebb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-03-25 22:36:53 +0530

    Create 3546_Equal_Sum_Grid_Partition_I.cpp

---
+++ 3546_Equal_Sum_Grid_Partition_I.cpp
@@ -0,0 +1,48 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        long long s = 0;
+        for (const auto& row : grid) {
+            for (int x : row) {
+                s += x;
+            }
+        }
+        if (s % 2 != 0) {
+            return false;
+        }
+        int m = grid.size(), n = grid[0].size();
+        long long pre = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int x : grid[i]) {
+                pre += x;
+            }
+            if (pre * 2 == s && i + 1 < m) {
+                return true;
+            }
+        }
+        pre = 0;
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m; ++i) {
+                pre += grid[i][j];
+            }
+            if (pre * 2 == s && j + 1 < n) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
