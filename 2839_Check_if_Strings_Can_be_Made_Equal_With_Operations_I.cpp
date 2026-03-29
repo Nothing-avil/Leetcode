3b9c3a27ec03001fe1c269d97a6fe22494053b6b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-03-29 22:30:53 +0530

    Create 2839_Check_if_Strings_Can_be_Made_Equal_With_Operations_I.cpp

---
+++ 2839_Check_if_Strings_Can_be_Made_Equal_With_Operations_I.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        vector<vector<int>> cnt(2, vector<int>(26, 0));
+        for (int i = 0; i < s1.size(); ++i) {
+            ++cnt[i & 1][s1[i] - 'a'];
+            --cnt[i & 1][s2[i] - 'a'];
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (cnt[0][i] || cnt[1][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
