45c8e8dfd5c7c32441d1abb8aa0de38d1bef5ebf
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-04 22:29:40 +0530

    Create 2075_Decode_the_Slanted_Ciphertext.cpp

---
+++ 2075_Decode_the_Slanted_Ciphertext.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        string ans;
+        int cols = encodedText.size() / rows;
+        for (int j = 0; j < cols; ++j) {
+            for (int x = 0, y = j; x < rows && y < cols; ++x, ++y) {
+                ans += encodedText[x * cols + y];
+            }
+        }
+        while (ans.size() && ans.back() == ' ') {
+            ans.pop_back();
+        }
+        return ans;
+    }
+};
