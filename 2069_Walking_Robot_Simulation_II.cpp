adfdbf79a90e9957bba78c20fdc2f753b0fec6de
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-07 23:09:47 +0530

    Create 2069_Walking_Robot_Simulation_II.cpp

---
+++ 2069_Walking_Robot_Simulation_II.cpp
@@ -0,0 +1,73 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Robot {
+public:
+    int mx, my, p, cur;
+    bool moved;
+
+    Robot(int width, int height) {
+        mx = width - 1;
+        my = height - 1;
+        p = 2 * mx + 2 * my;
+        cur = 0;
+        moved = false;
+    }
+
+    void step(int num) {
+        moved = true;
+        cur = (cur + num) % p;
+    }
+
+    vector<int> getPos() {
+        int d = cur;
+        int mx = this->mx, my = this->my;
+
+        if (0 <= d && d <= mx) {
+            return {d, 0};
+        }
+        if (mx < d && d <= mx + my) {
+            return {mx, d - mx};
+        }
+        if (mx + my < d && d <= 2 * mx + my) {
+            return {mx - (d - (mx + my)), my};
+        }
+        return {0, my - (d - (2 * mx + my))};
+    }
+
+    string getDir() {
+        int d = cur;
+        int mx = this->mx, my = this->my;
+
+        if (!moved) {
+            return "East";
+        }
+        if (1 <= d && d <= mx) {
+            return "East";
+        } else if (mx < d && d <= mx + my) {
+            return "North";
+        } else if (mx + my < d && d <= 2 * mx + my) {
+            return "West";
+        }
+        return "South";
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
