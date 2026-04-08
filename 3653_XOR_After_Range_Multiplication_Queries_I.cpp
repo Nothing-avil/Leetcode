23feba6e08067009003568a75d71d2300ea01534
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-08 21:50:35 +0530

    Create 3653_XOR_After_Range_Multiplication_Queries_I.cpp

---
+++ 3653_XOR_After_Range_Multiplication_Queries_I.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        const int mod = 1e9+7;
+        for(auto q : queries){
+            for(int j = q[0]; j <= q[1]; j+=q[2]){
+                nums[j] = (1ll * nums[j] * q[3]) % mod;
+            }
+        }
+        int answer = 0;
+        for(int i=0; i<nums.size(); i++){
+            answer ^= nums[i];
+        }
+        return answer;
+    }
+};
