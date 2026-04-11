62c2319a0b9b1ea43f966611e576efdb126a3962
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-11 23:45:07 +0530

    Create 3741_Minimum_Distance_Between_Three_Equal_Elements_II.cpp

---
+++ 3741_Minimum_Distance_Between_Three_Equal_Elements_II.cpp
@@ -0,0 +1,38 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        vector<int> pre(nums.size());
+        int answer = INT_MAX;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(nums[i]) == mp.end()){
+                mp[nums[i]] = i;
+                pre[i] = -1;
+            }
+            else{
+                pre[i] = mp[nums[i]];
+                mp[nums[i]] = i;
+            }
+        }
+        for(int i=nums.size() -1; i>=0; i--){
+            if(pre[i] != -1 && pre[pre[i]] != -1){
+                answer = min(answer, 2 * abs(pre[pre[i]] - i));
+            }
+        }
+        return answer == INT_MAX ? -1 : answer;
+    }
+};
