9000ec8fc0d4920e4e1459d26cdea757ff3916c3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-15 21:29:28 +0530

    Create 2515_Shortest_Distance_to_Target_String_in_a_Circular_Array.cpp

---
+++ 2515_Shortest_Distance_to_Target_String_in_a_Circular_Array.cpp
@@ -0,0 +1,28 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        int answer = INT_MAX;
+        for(int i=0; i<n; i++){
+            if(target == words[i]){
+                answer = min(answer, (startIndex - i + n) % n);
+                answer = min(answer, (i - startIndex + n) % n);
+            }
+        }
+        return answer == INT_MAX ? -1 : answer;
+    }
+};
