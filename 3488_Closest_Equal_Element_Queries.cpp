a39a8dbb94586ea209dd6adb2b8eb3d520b868f7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-16 22:47:23 +0530

    Create 3488_Closest_Equal_Element_Queries.cpp

---
+++ 3488_Closest_Equal_Element_Queries.cpp
@@ -0,0 +1,46 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        int m = n * 2;
+        vector<int> d(m, m);
+        unordered_map<int, int> left;
+        for (int i = 0; i < m; i++) {
+            int x = nums[i % n];
+            if (left.count(x)) {
+                d[i] = min(d[i], i - left[x]);
+            }
+            left[x] = i;
+        }
+        unordered_map<int, int> right;
+        for (int i = m - 1; i >= 0; i--) {
+            int x = nums[i % n];
+            if (right.count(x)) {
+                d[i] = min(d[i], right[x] - i);
+            }
+            right[x] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            d[i] = min(d[i], d[i + n]);
+        }
+        vector<int> ans;
+        for (int query : queries) {
+            ans.push_back(d[query] >= n ? -1 : d[query]);
+        }
+        return ans;
+    }
+};
