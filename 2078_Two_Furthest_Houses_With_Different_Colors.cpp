aeaffa84b6233571d5228739ada1f1631b2bece3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-20 21:58:48 +0530

    Create 2078_Two_Furthest_Houses_With_Different_Colors.cpp

---
+++ 2078_Two_Furthest_Houses_With_Different_Colors.cpp
@@ -0,0 +1,30 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int answer = 0;
+        int n = colors.size();
+        for(int i=0; i<n; i++){
+            if(colors[i] != colors[n - 1]){
+                answer = max(answer, abs(i - (n - 1)));
+            }
+            if(colors[i] != colors[0]){
+                answer = max(answer, i);
+            }
+        }
+        return answer;
+    }
+};
