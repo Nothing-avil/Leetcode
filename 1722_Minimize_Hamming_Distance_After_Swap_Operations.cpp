7bfd71c436109396aca022ea9b2e78d5dbb87d50
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-21 23:02:05 +0530

    Create 1722_Minimize_Hamming_Distance_After_Swap_Operations.cpp

---
+++ 1722_Minimize_Hamming_Distance_After_Swap_Operations.cpp
@@ -0,0 +1,39 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        vector<int> p(n);
+        iota(p.begin(), p.end(), 0);
+        function<int(int)> find = [&](int x) {
+            return x == p[x] ? x : p[x] = find(p[x]);
+        };
+        for (auto& a : allowedSwaps) {
+            p[find(a[0])] = find(a[1]);
+        }
+        unordered_map<int, unordered_map<int, int>> cnt;
+        for (int i = 0; i < n; ++i) {
+            ++cnt[find(i)][source[i]];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (--cnt[find(i)][target[i]] < 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
