3ddac7fada601c8e33e773a2d4b3fdd744398de8
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-04-26 23:58:35 +0530

    Create 1559_Detect_Cycles_in_2D_Grid.cpp

---
+++ 1559_Detect_Cycles_in_2D_Grid.cpp
@@ -0,0 +1,45 @@
+// ███████╗  █████╗  ███╗   ██╗ ██████╗   █████╗  ██████╗  ██████╗  ██╗  ██╗
+// ██╔════╝ ██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██║  ██║
+// ███████╗ ███████║ ██╔██╗ ██║ ██║  ██║ ███████║ ██████╔╝ ██████╔╝ ███████║
+// ╚════██║ ██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██║ ██╔═██╗  ██╔══██╗ ██╔══██║
+// ███████║ ██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ██║  ██╗ ██████╔╝ ██║  ██║
+// ╚══════╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+class Solution {
+public:
+    bool containsCycle(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n));
+        const vector<int> dirs = {-1, 0, 1, 0, -1};
+        auto dfs = [&](this auto&& dfs, int x, int y, int px, int py) -> bool {
+            vis[x][y] = true;
+            for (int k = 0; k < 4; ++k) {
+                int nx = x + dirs[k], ny = y + dirs[k + 1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                    if (grid[nx][ny] != grid[x][y] || (nx == px && ny == py)) {
+                        continue;
+                    }
+                    if (vis[nx][ny] || dfs(nx, ny, x, y)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (!vis[i][j] && dfs(i, j, -1, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
